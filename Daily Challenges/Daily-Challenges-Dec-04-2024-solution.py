a46c8da64fa449fbc65881392648b3b279faf7eb
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-09-08 15:50:55 +0300

    D-2825

---
+++ Daily Challenges/Daily-Challenges-Dec-04-2024-solution.py
@@ -0,0 +1,24 @@
+# 🔁 LeetCode 2825 - Make String a Subsequence Using Cyclic Increments
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 2825](https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments)
+
+# 🧠 Problem Description
+# [Github LeetCode 2825 - Make String a Subsequence Using Cyclic Increments](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2825.%20Make%20String%20a%20Subsequence%20Using%20Cyclic%20Increments)
+
+class Solution(object):
+    def canMakeSubsequence(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: bool
+        """
+        n, m = len(str1), len(str2)
+        j = 0
+        for i in range(n):
+            if str1[i] == str2[j] or (chr(ord(str1[i]) + 1) if str1[i] != 'z' else 'a') == str2[j]:
+                j += 1
+            if j == m:
+                return True
+        return False
+        
