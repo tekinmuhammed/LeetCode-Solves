5a2eeccd7d9a7fda1d1672f11138321a87eb2b11
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-09-09 15:43:39 +0300

    D-2593

--- Daily Challenges/Daily-Challenges-Dec-13-2024-solution.py
+++ Daily Challenges/Daily-Challenges-Dec-13-2024-solution.py
@@ -1,21 +1,28 @@
-# 🟨 LeetCode 2558 - Take Gifts From the Richest Pile
+# 🟨 LeetCode 2593 - Find Score of an Array After Marking All Elements
 
-# **Difficulty:** Easy  
-# **Problem Link:** [LeetCode 2558](https://leetcode.com/problems/take-gifts-from-the-richest-pile/)
+#  **Difficulty:** Medium  
+#  **Problem Link:** [LeetCode 2593](https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/)
 
 # 🧠 Problem Description 
-# [Github LeetCode 2558 - Take Gifts From the Richest Pile](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2558.%20Take%20Gifts%20From%20the%20Richest%20Pile)
-
-import math
+# [Github LeetCode 2593 - Find Score of an Array After Marking All Elements](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2593.%20Find%20Score%20of%20an%20Array%20After%20Marking%20All%20Elements)
 
 class Solution(object):
-    def pickGifts(self, gifts, k):
+    def findScore(self, nums):
         """
-        :type gifts: List[int]
-        :type k: int
+        :type nums: List[int]
         :rtype: int
         """
-        for _ in range(k):
-            max_index = gifts.index(max(gifts))
-            gifts[max_index] = int(math.floor(math.sqrt(gifts[max_index])))
-        return sum(gifts)
+        score = 0
+        n = len(nums)
+        marked = [False] * n
+        indexed_nums = [(nums[i], i) for i in range(n)]
+        indexed_nums.sort()
+        for value, index in indexed_nums:
+            if not marked[index]:
+                score += value
+                marked[index] = True
+                if index > 0:
+                    marked[index - 1] = True
+                if index <n - 1:
+                    marked[index + 1] = True
+        return score
