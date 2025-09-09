d5f0e7e9b05b3b83549eec46a0530e18350a3948
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-09-09 15:45:20 +0300

    D-2762

---
+++ Daily Challenges/Daily-Challenges-Dec-14-2024-solution.py
@@ -0,0 +1,35 @@
+# 🟨 LeetCode 2762 - Continuous Subarrays
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 2762](https://leetcode.com/problems/continuous-subarrays/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 2762 - Continuous Subarrays](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2762.%20Continuous%20Subarrays)
+
+from collections import deque
+
+class Solution(object):
+    def continuousSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        min_deque = deque()
+        max_deque = deque()
+        left = 0
+        count = 0
+        for right in range(len(nums)):
+            while min_deque and nums[min_deque[-1]] >= nums[right]:
+                min_deque.pop()
+            while max_deque and nums[max_deque[-1]] <= nums[right]:
+                max_deque.pop()
+            min_deque.append(right)
+            max_deque.append(right)
+            while nums[max_deque[0]] - nums[min_deque[0]] > 2:
+                left += 1
+                if min_deque[0] < left:
+                    min_deque.popleft()
+                if max_deque[0] < left:
+                    max_deque.popleft()
+            count += right - left + 1
+        return count
