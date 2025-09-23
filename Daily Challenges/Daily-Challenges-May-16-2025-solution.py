d9e87b24bc9cde34e82c1196d9caa62f3a07453b
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-09-23 15:55:53 +0300

    D-2901

---
+++ Daily Challenges/Daily-Challenges-May-16-2025-solution.py
@@ -0,0 +1,44 @@
+# 🧠 LeetCode 2901 - Longest Unequal Adjacent Groups Subsequence II
+
+# **Difficulty:** Hard  
+# **Problem Link:** [LeetCode 2901](https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-ii)
+
+# 🧠 Problem Description 
+# [Github LeetCode 2901 - Longest Unequal Adjacent Groups Subsequence II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2901.%20Longest%20Unequal%20Adjacent%20Groups%20Subsequence%20II)
+
+class Solution(object):
+    def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        """
+        :type words: List[str]
+        :type groups: List[int]
+        :rtype: List[str]
+        """
+        def hammingValid(word1, word2):
+            if len(word1) != len(word2):
+                return False
+            diff = 0
+            for w1, w2 in zip(word1, word2):
+                if w1!=w2:
+                    diff+=1
+            return diff ==1
+
+        n = len(words)
+        dp = [1] * n
+        parent = [-1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if hammingValid(words[i], words[j]) and dp[j]+1 > dp[i] and groups[i] != groups[j]:
+                    dp[i] = dp[j] + 1
+                    parent[i] = j
+            
+        idx = dp.index(max(dp))
+
+        res = []
+
+        while idx != -1:
+            res.append(words[idx])
+            idx = parent[idx]
+        res.reverse()
+
+        return res
