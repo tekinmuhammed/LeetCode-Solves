3420e12b6b10f6c9b9355b1d1a4a58630eb33adb
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-01 15:06:55 +0300

    D-2294-2

--- Daily Challenges/Daily-Challenges-Jun-19-2025-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-19-2025-solution.py
@@ -5,3 +5,20 @@
 
 # 🧠 Problem Description 
 # [Github LeetCode 2294. Partition Array Such That Maximum Difference Is K](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2294.%20Partition%20Array%20Such%20That%20Maximum%20Difference%20Is%20K)
+
+class Solution(object):
+    def partitionArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        count = 1
+        start = nums[0]
+
+        for num in nums:
+            if num - start > k:
+                count += 1
+                start = num
+        return count
