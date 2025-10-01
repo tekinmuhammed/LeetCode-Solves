ee1f32f7ecfedc234b9b6d67a3f40b227440af82
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-01 15:06:24 +0300

    D-2294

---
+++ Daily Challenges/Daily-Challenges-Jun-19-2025-solution.py
@@ -0,0 +1,7 @@
+# 2294. Partition Array Such That Maximum Difference Is K
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 2294](https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k)
+
+# 🧠 Problem Description 
+# [Github LeetCode 2294. Partition Array Such That Maximum Difference Is K](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2294.%20Partition%20Array%20Such%20That%20Maximum%20Difference%20Is%20K)
