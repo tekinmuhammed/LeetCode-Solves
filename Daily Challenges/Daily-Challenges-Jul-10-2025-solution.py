fc8af135f0ba70517410b8cb5d47b96568b2a7c0
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-02 21:30:50 +0300

    D-3440

---
+++ Daily Challenges/Daily-Challenges-Jul-10-2025-solution.py
@@ -0,0 +1,35 @@
+# 3440. Reschedule Meetings for Maximum Free Time II
+
+# **Difficulty:** Hard  
+# **Link:** [LeetCode 3440](https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-ii)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3440. Reschedule Meetings for Maximum Free Time II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3440.%20Reschedule%20Meetings%20for%20Maximum%20Free%20Time%20II)
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        n = len(startTime)
+        t1 = 0
+        t2 = 0
+        res = 0
+
+        for i in range(n):
+            left1 = 0 if i == 0 else endTime[i-1]
+            right1 = eventTime if i == n-1 else startTime[i+1]
+
+            if endTime[i] - startTime[i] <= t1:
+                res = max(res, right1 - left1)
+            else:
+                res = max(res, right1 - left1 - (endTime[i] - startTime[i]))
+            t1 = max(t1, startTime[i] - (0 if i == 0 else endTime[i-1]))
+            
+            left2 = 0 if i == n-1 else endTime[n-i-2]
+            right2 = eventTime if i == 0 else startTime[n-i]
+
+            if endTime[n-i-1] - startTime[n-i-1] <= t2:
+                res = max(res, right2 - left2)
+            else:
+                res = max(res, right2 - left2 - (endTime[n-i-1] - startTime[n-i-1]))
+            t2 = max(t2, (eventTime if i==0 else startTime[n-i]) - endTime[n-i-1])
+
+        return res
