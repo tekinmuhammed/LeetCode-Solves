5cc89fa72b165b1ee2a2c32aa4a4a117b3275d67
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-12 17:03:27 +0300

    D-1233-2

--- Daily Challenges/Daily-Challenges-Jul-19-2025-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-19-2025-solution.py
@@ -4,7 +4,7 @@
 # **Problem Link:** [LeetCode 1233](https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/)
 
 # 🧠 Problem Description 
-# [Github LeetCode 3136. Valid Word](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1233.%20Remove%20Sub-Folders%20from%20the%20Filesystem%20II)
+# [Github LeetCode 1233. Remove Sub-Folders from the Filesystem](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1233.%20Remove%20Sub-Folders%20from%20the%20Filesystem%20II)
 
 class Solution(object):
     def removeSubfolders(self, folder):
