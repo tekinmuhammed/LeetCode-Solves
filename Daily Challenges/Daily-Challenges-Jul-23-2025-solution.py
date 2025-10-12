1a0c8209760a5425249c3eb2b54711d7ec43c9ed
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-12 16:47:00 +0300

    D-1717

---
+++ Daily Challenges/Daily-Challenges-Jul-23-2025-solution.py
@@ -0,0 +1,32 @@
+# 1717. Maximum Score From Removing Substrings
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 1717](https://leetcode.com/problems/maximum-score-from-removing-substrings/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3136. Valid Word](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1717.%20Maximum%20Score%20From%20Removing%20Substrings)
+
+class Solution(object):
+    def maximumGain(self, s, x, y):
+        def remove_pattern(s, a, b, point):
+            stack = []
+            score = 0
+            for ch in s:
+                if stack and stack[-1] == a and ch == b:
+                    stack.pop()
+                    score += point
+                else:
+                    stack.append(ch)
+            return "".join(stack), score
+
+        total_score = 0
+
+        # Önce en yüksek puanlı işlemi yap
+        if x > y:
+            s, score1 = remove_pattern(s, 'a', 'b', x)
+            s, score2 = remove_pattern(s, 'b', 'a', y)
+        else:
+            s, score1 = remove_pattern(s, 'b', 'a', y)
+            s, score2 = remove_pattern(s, 'a', 'b', x)
+
+        return score1 + score2
