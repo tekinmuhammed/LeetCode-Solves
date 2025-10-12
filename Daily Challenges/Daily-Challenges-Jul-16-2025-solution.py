00c104390603a09e336fcc66a54c1a214446cd5a
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-12 16:37:49 +0300

    D-3201

---
+++ Daily Challenges/Daily-Challenges-Jul-16-2025-solution.py
@@ -0,0 +1,71 @@
+# 3201. Find the Maximum Length of Valid Subsequence I
+
+# **Difficulty:** Easy  
+# **Link:** [LeetCode 3201](https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3136. Valid Word](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3201.%20Find%20the%20Maximum%20Length%20of%20Valid%20Subsequence%20I)
+
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+
+        mx = 0
+        cnt = 0
+        for e in nums:
+            if e % 2 == 0:
+                cnt += 1
+            else:
+                continue
+        
+        mx = max(mx, cnt)
+
+        cnt = 0
+        for e in nums:
+            if e % 2 == 1:
+                cnt += 1
+            else:
+                continue
+        
+        mx = max(mx, cnt)
+
+        print(mx)
+
+        flag = True
+
+        cnt = 0
+        for e in nums:
+            if flag:
+                if e % 2 == 1:
+                    cnt += 1
+                    flag = False
+                else:
+                    continue
+            else:
+                if e % 2 == 0:
+                    cnt += 1
+                    flag = True
+                else:
+                    continue
+        
+        mx = max(mx, cnt)
+        
+        flag = False
+
+        cnt = 0
+        for e in nums:
+            if flag:
+                if e % 2 == 1:
+                    cnt += 1
+                    flag = False
+                else:
+                    continue
+            else:
+                if e % 2 == 0:
+                    cnt += 1
+                    flag = True
+                else:
+                    continue
+        
+        mx = max(mx, cnt)
+
+        return (mx)
