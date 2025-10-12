b4d822a9d9ae051529206c9f7099ec83e1fefc6f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-12 16:53:38 +0300

    D-2044-2

---
+++ Daily Challenges/Daily-Challenges-Jul-28-2025-solution.py
@@ -0,0 +1,28 @@
+# 2044. Count Number of Maximum Bitwise-OR Subsets
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 2044](https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3136. Valid Word](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2044.%20Count%20Number%20of%20Maximum%20Bitwise-OR%20Subsets-2)
+
+class Solution(object):
+    def countMaxOrSubsets(self, nums):
+        self.max_or = 0
+        self.count = 0
+
+        def dfs(i, cur_or):
+            if i == len(nums):
+                if cur_or > self.max_or:
+                    self.max_or = cur_or
+                    self.count = 1
+                elif cur_or == self.max_or:
+                    self.count += 1
+                return
+            # Dahil et
+            dfs(i + 1, cur_or | nums[i])
+            # Dahil etme
+            dfs(i + 1, cur_or)
+
+        dfs(0, 0)
+        return self.count
