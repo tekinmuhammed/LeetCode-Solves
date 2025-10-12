b26f37ed7dc2c6bbe505cffd2889449944cdd28c
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-12 16:48:14 +0300

    D-2322

---
+++ Daily Challenges/Daily-Challenges-Jul-24-2025-solution.py
@@ -0,0 +1,50 @@
+# 2322. Minimum Score After Removals on a Tree
+
+# **Difficulty:** Hard  
+# **Problem Link:** [LeetCode 2322](https://leetcode.com/problems/minimum-score-after-removals-on-a-tree/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3136. Valid Word](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/2322.%20Minimum%20Score%20After%20Removals%20on%20a%20Tree)
+
+class Solution:
+    def calc(self, part1: int, part2: int, part3: int) -> int:
+        return max(part1, part2, part3) - min(part1, part2, part3)
+
+    def minimumScore(self, nums: List[int], edges: List[List[int]]) -> int:
+        n = len(nums)
+        e = [[] for _ in range(n)]
+        for u, v in edges:
+            e[u].append(v)
+            e[v].append(u)
+
+        total = 0
+        for x in nums:
+            total ^= x
+
+        res = float("inf")
+
+        def dfs2(x: int, f: int, oth: int, anc: int) -> int:
+            son = nums[x]
+            for y in e[x]:
+                if y == f:
+                    continue
+                son ^= dfs2(y, x, oth, anc)
+            if f == anc:
+                return son
+            nonlocal res
+            res = min(res, self.calc(oth, son, total ^ oth ^ son))
+            return son
+
+        def dfs(x: int, f: int) -> int:
+            son = nums[x]
+            for y in e[x]:
+                if y == f:
+                    continue
+                son ^= dfs(y, x)
+            for y in e[x]:
+                if y == f:
+                    dfs2(y, x, son, x)
+            return son
+
+        dfs(0, -1)
+        return res
