c1c3bd9665610c67ee2c7e5f5cff6c12548b379c
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-13 11:39:18 +0300

    D-808

---
+++ Daily Challenges/Daily-Challenges-Aug-08-2025-solution.py
@@ -0,0 +1,28 @@
+# LeetCode 808 - Soup Servings
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 808](https://leetcode.com/problems/soup-servings/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode LeetCode 808 - Soup Servings](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/808.%20Soup%20Servings)
+
+class Solution:
+    def soupServings(self, n: int) -> float:
+        m = ceil(n / 25)
+        dp = {}
+
+        def calculate_dp(i, j):
+            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
+                    dp[max(0, i - 2)][max(0, j - 2)]
+                    + dp[i - 1][max(0, j - 3)]) / 4
+
+        dp[0] = {0: 0.5}
+        for k in range(1, m + 1):
+            dp[0][k] = 1
+            dp[k] = {0: 0}
+            for j in range(1, k + 1):
+                dp[j][k] = calculate_dp(j, k)
+                dp[k][j] = calculate_dp(k, j)
+            if dp[k][k] > 1 - 1e-5:
+                return 1
+        return dp[m][m]
