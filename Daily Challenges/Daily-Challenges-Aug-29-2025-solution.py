c132dc493dd2b2d4a9bf4f7c599db469d5c9f424
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-15 14:51:50 +0300

    D-3021

---
+++ Daily Challenges/Daily-Challenges-Aug-29-2025-solution.py
@@ -0,0 +1,11 @@
+# 3021. Alice and Bob Playing Flower Game
+
+# **Difficulty:** Medium
+# **Link:** [LeetCode 3021](https://leetcode.com/problems/alice-and-bob-playing-flower-game/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3021. Alice and Bob Playing Flower Game](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3021.%20Alice%20and%20Bob%20Playing%20Flower%20Game)
+
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return (m * n) // 2
