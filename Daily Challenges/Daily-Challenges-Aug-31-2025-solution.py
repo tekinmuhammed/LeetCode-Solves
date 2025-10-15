9d806e8a3b24464fb58c976c33adb342fac64459
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-15 14:55:00 +0300

    D-37

---
+++ Daily Challenges/Daily-Challenges-Aug-31-2025-solution.py
@@ -0,0 +1,46 @@
+# 37. Sudoku Solver
+
+# **Difficulty:** Hard  
+# **Link:** [LeetCode 37](https://leetcode.com/problems/sudoku-solver/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 37. Sudoku Solver](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/37.%20Sudoku%20Solver)
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empties = []
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    empties.append((r, c))
+                else:
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    box_index = (r // 3) * 3 + (c // 3)
+                    boxes[box_index].add(val)
+        
+        def backtrack(index=0):
+            if index == len(empties):
+                return True
+            r, c = empties[index]
+            box_index = (r // 3) * 3 + (c // 3)
+            for ch in map(str, range(1, 10)):
+                if (ch not in rows[r] and ch not in cols[c] and ch not in boxes[box_index]):
+                    board[r][c] = ch
+                    rows[r].add(ch)
+                    cols[c].add(ch)
+                    boxes[box_index].add(ch)
+
+                    if backtrack(index + 1):
+                        return True
+                    board[r][c] = '.'
+                    rows[r].remove(ch)
+                    cols[c].remove(ch)
+                    boxes[box_index].remove(ch)
+            return False
+        backtrack()
