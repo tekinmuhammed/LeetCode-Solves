90ee2971f1e2c699ee37dc84e77afb6430746f54
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-15 14:41:14 +0300

    D-3195

---
+++ Daily Challenges/Daily-Challenges-Aug-22-2025-solution.py
@@ -0,0 +1,12 @@
+# 3195. Find the Minimum Area to Cover All Ones I
+
+# **Difficulty:** Easy  
+# **Link:** [LeetCode 3195](https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i/)  
+
+# 🧠 Problem Description 
+# [Github LeetCode 3195. Find the Minimum Area to Cover All Ones I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3195.%20Find%20the%20Minimum%20Area%20to%20Cover%20All%20Ones%20I)
+
+class Solution:
+    def minimumArea(self, g: List[List[int]]) -> int:
+        f = lambda g,q:next(k for k in range(len(g))[::q] if any(g[k]))
+        return (f(g,-1)-f(g,1)+1)*(f(g:=[*zip(*g)],-1)-f(g,1)+1)
