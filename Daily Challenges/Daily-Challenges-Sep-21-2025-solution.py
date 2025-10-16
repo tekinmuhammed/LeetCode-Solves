4ebbfd71095ba448c8ebae77edf22a2da1b96eb7
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-16 18:47:30 +0300

    D-1912

---
+++ Daily Challenges/Daily-Challenges-Sep-21-2025-solution.py
@@ -0,0 +1,51 @@
+# 🎬 LeetCode 1912 - Design Movie Rental System
+
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 1912](https://leetcode.com/problems/design-movie-rental-system)
+
+# 🧠 Problem Description
+# [Github LeetCode 1912 - Design Movie Rental System](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1912.%20Design%20Movie%20Rental%20System)
+
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        self.movies = defaultdict(SortedList)
+        self.sm_p = {}
+        self.rented = SortedList()
+
+        for s, m, p in entries:
+            self.movies[m].add((p, s))
+            self.sm_p[(s, m)] = p
+
+    def search(self, movie: int) -> List[int]:
+        res = []
+        n = len(self.movies[movie])
+        for i in range(5):
+            if i == n : break
+            res.append(self.movies[movie][i][1])
+        return res
+
+    def rent(self, shop: int, movie: int) -> None:
+        p = self.sm_p[(shop, movie)]
+        self.movies[movie].discard((p, shop))
+        self.rented.add((p, shop, movie))
+
+    def drop(self, shop: int, movie: int) -> None:
+        p = self.sm_p[(shop, movie)]
+        self.movies[movie].add((p, shop))
+        self.rented.discard((p, shop, movie))
+
+    def report(self) -> List[List[int]]:
+        res = []
+        n = len(self.rented)
+        for  i in  range(5):
+            if  i == n : break
+            res.append([self.rented[i][1], self.rented[i][2]])
+        return res
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
