395d974c65694e0a33949e33ff9e49fc345a66fe
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-10-20 21:39:12 +0300

    D-812

---
+++ Daily Challenges/Daily-Challenges-Sep-27-2025-solution.py
@@ -0,0 +1,16 @@
+# 812. Largest Triangle Area
+
+# **Difficulty:** Easy  
+# **Link:** [LeetCode 812](https://leetcode.com/problems/largest-triangle-area/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 812. Largest Triangle Area](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/812.%20Largest%20Triangle%20Area)
+
+class Solution(object):
+    def largestTriangleArea(self, points):
+        def area(p, q, r):
+            return .5 * abs(p[0]*q[1]+q[0]*r[1]+r[0]*p[1]
+                           -p[1]*q[0]-q[1]*r[0]-r[1]*p[0])
+
+        return max(area(*triangle)
+            for triangle in itertools.combinations(points, 3))
