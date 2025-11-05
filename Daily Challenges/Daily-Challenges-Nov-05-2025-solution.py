10e528d3a4ad74b473e67f07468819666ffeced1
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-11-05 14:52:37 +0300

    D-3321

--- Daily Challenges/Daily-Challenges-Nov-05-2025-solution.py
+++ Daily Challenges/Daily-Challenges-Nov-05-2025-solution.py
@@ -1,9 +1,9 @@
-# 💡 LeetCode 3321 — Find X-Sum of All K-Long Subarrays II
+# 💡 LeetCode 3321 — Find X-Sum of All K-Long Subarrays II 
 
-# **Difficulty:** Hard
+# **Difficulty:** Hard 
 # **Problem Link:** [LeetCode 3321](https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-ii/description/)
 
-# 🧠 Problem Description
+# 🧠 Problem Description 
 # [Github LeetCode 3321 — Find X-Sum of All K-Long Subarrays II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/3321.%20Find%20X-Sum%20of%20All%20K-Long%20Subarrays%20II)
 
 from collections import Counter
@@ -23,10 +23,10 @@ class Solution(object):
             sub = nums[i:i + k]
             freq = Counter(sub)
             
-            # En sık görülen x elemanı bul (frekans ve değer sırasına göre)
+            # En sık görülen x elemanı bul (frekans ve değer sırasına göre) 
             most_common = sorted(freq.items(), key=lambda a: (-a[1], -a[0]))[:x]
             
-            # Bu elemanların toplam katkısını hesapla
+            # Bu elemanların toplam katkısını hesapla  
             s = 0
             for num, count in most_common:
                 s += num * count
