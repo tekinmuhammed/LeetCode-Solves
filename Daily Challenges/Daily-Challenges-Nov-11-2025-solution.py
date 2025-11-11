bbd4c56942dd09ac89e742e30837c4c6d6e9ea72
Muhammed Tekin <fetih.tekin.7@gmail.com>
2025-11-11 12:30:40 +0300

    D-474

---
+++ Daily Challenges/Daily-Challenges-Nov-11-2025-solution.py
@@ -0,0 +1,29 @@
+# 🔢 474. Ones and Zeroes
+
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 474](https://leetcode.com/problems/ones-and-zeroes/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 474. Ones and Zeroes](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/474.%20Ones%20and%20Zeroes)
+
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # DP tablosu: dp[i][j] -> en fazla i adet 0 ve j adet 1 ile oluşturulabilecek maksimum alt küme boyutu
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+
+            # Geriye doğru güncelleme (0/1 knapsack mantığı)
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
+
+        return dp[m][n]
