9d165fe2e02407d633626e9789781b818f4cdf0f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-01-21 21:17:00 +0300

    D-3315

--- Daily Challenges/Daily-Challenges-Jan-21-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jan-21-2026-solution.py
@@ -1,6 +1,6 @@
 ## 3315. Construct the Minimum Bitwise Array II
 
-# **Difficulty:** Medium  
+# **Difficulty:** Medium
 # **Problem Link:** [LeetCode 3315](https://leetcode.com/problems/construct-the-minimum-bitwise-array-ii/description/)
 
 # 🧠 Problem Description
@@ -11,21 +11,21 @@ class Solution:
         ans = []
         for x in nums:
             if x == 2:
-                # 2 sayısı (binary 10) için koşulu sağlayan bir değer yoktur.
+                # 2 sayısı (binary 10) için koşulu sağlayan bir değer yoktur
                 ans.append(-1)
             else:
-                # x asal sayı olduğu için tektir (2 hariç).
+                # x asal sayı olduğu için tektir (2 hariç)
                 # Amacımız x'in sonundaki 1 bloğunun en büyük basamağındaki 1'i 0 yapmaktır.
                 
                 # 1. Adım: x + 1 ile son 1 bloğunu sıfırla ve bir solundaki 0'ı 1 yap.
                 # Örn: x = 23 (10111) -> x + 1 = 24 (11000)
                 next_val = x + 1
                 
-                # 2. Adım: Değişen biti (Lowbit) bul.
+                # 2. Adım: Değişen biti (Lowbit) bul
                 # 24 (11000) -> Lowbit 8 (1000)
                 lowbit = next_val & -next_val
                 
-                # 3. Adım: Lowbit'in yarısını x'ten çıkar.
+                # 3. Adım: Lowbit'in yarısını x'ten çıkar
                 # 8 >> 1 = 4.
                 # 23 - 4 = 19. (19 -> 10011. 19 OR 20 = 23. Doğru ve minimum.)
                 ans.append(x - (lowbit >> 1))
