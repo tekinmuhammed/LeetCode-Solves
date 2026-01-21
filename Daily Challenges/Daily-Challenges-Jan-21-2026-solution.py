5bd794308755c80fa8ffe6421a6ef9085f8e02cb
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-01-21 21:21:26 +0300

    D-3315

--- Daily Challenges/Daily-Challenges-Jan-21-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jan-21-2026-solution.py
@@ -15,9 +15,9 @@ class Solution:
                 ans.append(-1)
             else:
                 # x asal sayı olduğu için tektir (2 hariç)
-                # Amacımız x'in sonundaki 1 bloğunun en büyük basamağındaki 1'i 0 yapmaktır.
+                # Amacımız x'in sonundaki 1 bloğunun en büyük basamağındaki 1'i 0 yapmaktır
                 
-                # 1. Adım: x + 1 ile son 1 bloğunu sıfırla ve bir solundaki 0'ı 1 yap.
+                # 1. Adım: x + 1 ile son 1 bloğunu sıfırla ve bir solundaki 0'ı 1 yap
                 # Örn: x = 23 (10111) -> x + 1 = 24 (11000)
                 next_val = x + 1
                 
@@ -27,7 +27,7 @@ class Solution:
                 
                 # 3. Adım: Lowbit'in yarısını x'ten çıkar
                 # 8 >> 1 = 4.
-                # 23 - 4 = 19. (19 -> 10011. 19 OR 20 = 23. Doğru ve minimum.)
+                # 23 - 4 = 19. (19 -> 10011. 19 OR 20 = 23. Doğru ve minimum)
                 ans.append(x - (lowbit >> 1))
                 
         return ans
