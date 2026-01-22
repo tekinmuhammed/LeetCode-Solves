d380f97414463cb99b52e37ae748b8bfea525989
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-01-22 17:14:16 +0300

    D-3507

--- Daily Challenges/Daily-Challenges-Jan-22-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jan-22-2026-solution.py
@@ -5,3 +5,28 @@
 
 # 🧠 Problem Description
 # [Github LeetCode 3507. Minimum Pair Removal to Sort Array I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3507.%20Minimum%20Pair%20Removal%20to%20Sort%20Array%20I)
+
+class Solution(object):
+    def minimumPairRemoval(self, nums):
+        ops = 0
+
+        def is_sorted(arr):
+            for i in range(1, len(arr)):
+                if arr[i] < arr[i - 1]:
+                    return False
+            return True
+
+        while not is_sorted(nums):
+            min_sum = float('inf')
+            idx = 0
+
+            for i in range(len(nums) - 1):
+                s = nums[i] + nums[i + 1]
+                if s < min_sum:
+                    min_sum = s
+                    idx = i
+
+            nums = nums[:idx] + [min_sum] + nums[idx + 2:]
+            ops += 1
+
+        return ops
