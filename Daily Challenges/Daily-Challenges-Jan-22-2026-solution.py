a200dc70aa605a1bd88e4cb09d2a5faae9c5e921
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-01-22 17:13:44 +0300

    D-3507

---
+++ Daily Challenges/Daily-Challenges-Jan-22-2026-solution.py
@@ -0,0 +1,7 @@
+## 3507. Minimum Pair Removal to Sort Array I
+
+# **Difficulty:** Easy  
+# **Problem Link:** [LeetCode 3507](https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3507. Minimum Pair Removal to Sort Array I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3507.%20Minimum%20Pair%20Removal%20to%20Sort%20Array%20I)
