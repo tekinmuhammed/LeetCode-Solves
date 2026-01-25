51edf1265c486a3d07a10375b90af8ceeac68923
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-01-25 22:34:09 +0300

    D-1984

--- Daily Challenges/Daily-Challenges-Jan-25-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jan-25-2026-solution.py
@@ -1,9 +1,9 @@
-## 1984. Minimum Difference Between Highest and Lowest of K Scores
+## 1984. Minimum Difference Between Highest and Lowest of K Scores 
 
-# **Difficulty:** Easy
+# **Difficulty:** Easy 
 # **Problem Link:** [LeetCode 1984](https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/)
 
-# 🧠 Problem Description
+# 🧠 Problem Description 
 # [Github LeetCode 1984. Minimum Difference Between Highest and Lowest of K Scores](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1984.%20Minimum%20Difference%20Between%20Highest%20and%20Lowest%20of%20K%20Scores)
 
 class Solution(object):
