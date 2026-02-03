39e689c40fd2d1bd92c1f0aef4c9bfe2a547af66
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-03 15:56:35 +0300

    3637

---
+++ Easy/3637. Trionic Array I/3637. Trionic Array I.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isTrionic(self, nums):
+        n = len(nums)
+        i = 0
+
+        # 1) strictly increasing
+        while i + 1 < n and nums[i] < nums[i + 1]:
+            i += 1
+        if i == 0 or i == n - 1:
+            return False
+
+        # 2) strictly decreasing
+        while i + 1 < n and nums[i] > nums[i + 1]:
+            i += 1
+        if i == n - 1:
+            return False
+
+        # 3) strictly increasing
+        while i + 1 < n and nums[i] < nums[i + 1]:
+            i += 1
+
+        return i == n - 1
