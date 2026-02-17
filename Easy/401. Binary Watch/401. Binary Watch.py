eed3cfb8f71151c1bbd53769f1b27ab3d8239d3a
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-17 17:52:02 +0300

    401

---
+++ Easy/401. Binary Watch/401. Binary Watch.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        result = []
+        
+        for h in range(12):
+            for m in range(60):
+                if (bin(h).count("1") + bin(m).count("1")) == turnedOn:
+                    result.append(f"{h}:{m:02d}")
+        
+        return result
