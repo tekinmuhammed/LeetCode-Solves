5d315c6622f1e846713b605a2af8f416a484cf3f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-22 16:36:33 +0300

    D-868

--- Daily Challenges/Daily-Challenges-Feb-22-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Feb-22-2026-solution.py
@@ -1,4 +1,7 @@
 # 868. Binary Gap
 
-**Difficulty:** Easy  
-**Link:** [LeetCode 868](https://leetcode.com/problems/binary-gap/description/)
+# **Difficulty:** Easy  
+# **Link:** [LeetCode 868](https://leetcode.com/problems/binary-gap/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 868. Binary Gap](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/868.%20Binary%20Gap)
