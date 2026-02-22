005a7d7651668a664caea1024e850046a6f70132
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-22 16:37:22 +0300

    D-868

--- Daily Challenges/Daily-Challenges-Feb-22-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Feb-22-2026-solution.py
@@ -5,3 +5,17 @@
 
 # 🧠 Problem Description
 # [Github LeetCode 868. Binary Gap](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/868.%20Binary%20Gap)
+
+class Solution(object):
+    def binaryGap(self, n):
+        b = bin(n)[2:]   # binary string
+        last = -1
+        max_gap = 0
+
+        for i in range(len(b)):
+            if b[i] == '1':
+                if last != -1:
+                    max_gap = max(max_gap, i - last)
+                last = i
+
+        return max_gap
