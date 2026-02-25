7af9b42f69369ca36c929d16fe3a29d24ad5c41a
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-25 13:19:03 +0300

    D-1356

---
+++ Daily Challenges/Daily-Challenges-Feb-25-2026-solution.py
@@ -0,0 +1,26 @@
+# 1356. Sort Integers by The Number of 1 Bits
+
+**Difficulty:** Easy  
+**Problem Link:** [LeetCode 1356](https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1022. Sum of Root To Leaf Binary Numbers](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1022.%20Sum%20of%20Root%20To%20Leaf%20Binary%20Numbers)
+
+class Solution(object):
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        # arr dizisini yerinde (in-place) sıralıyoruz.
+        # Sıralama kriteri olarak bir tuple veriyoruz: 
+        # 1. Eleman: Sayının ikili gösterimindeki '1'lerin sayısı
+        # 2. Eleman: Sayının kendi değeri (eşitlik durumunda tie-breaker olarak kullanılır)
+        arr.sort(key=lambda x: (bin(x).count('1'), x))
+        
+        return arr
+
+        # Not: Python 3.10 ve üzeri sürümlerde bin(x).count('1') yerine 
+        # doğrudan x.bit_count() kullanılarak daha performanslı bir çözüm de yazılabilir:
+        # arr.sort(key=lambda x: (x.bit_count(), x))
+        # return arr
