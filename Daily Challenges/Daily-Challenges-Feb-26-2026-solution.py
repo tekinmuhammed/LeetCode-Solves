267a24f1f552e07c579981f7e611735baf5d6871
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-26 12:06:13 +0300

    D-1404

--- Daily Challenges/Daily-Challenges-Feb-26-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Feb-26-2026-solution.py
@@ -1,3 +1,8 @@
+# 🔐 LeetCode 1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+**Difficulty:** Medium  
+**Problem Link:** [LeetCode 1404](https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/)
+
 class Solution(object):
     def numSteps(self, s):
         """
