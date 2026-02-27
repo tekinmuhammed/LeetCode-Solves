5ecff7d912f22b53a2209fad14ee8e5bed88e96d
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-02-27 16:13:04 +0300

    D-3666

--- Daily Challenges/Daily-Challenges-Feb-27-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Feb-27-2026-solution.py
@@ -1,3 +1,8 @@
+# 3666. Minimum Operations to Equalize Binary String
+
+# **Difficulty:** Hard
+# **Link:** [LeetCode 3666](https://leetcode.com/problems/minimum-operations-to-equalize-binary-string/description/)
+
 import math
 
 class Solution:
