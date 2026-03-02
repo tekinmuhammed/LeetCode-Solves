85a55a95e70724ef63cbb702f9abe29c113c50de
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-02 18:11:00 +0300

    D-1536

--- Daily Challenges/Daily-Challenges-Mar-02-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-02-2026-solution.py
@@ -1,3 +1,7 @@
+# 1536. Minimum Swaps to Arrange a Binary Grid
+
+**Difficulty:** Medium  
+**Problem Link:** [LeetCode 1536](https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/description/)
 
 # 🧠 Problem Description
 # [Github LeetCode 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1689.%20Partitioning%20Into%20Minimum%20Number%20Of%20Deci-Binary%20Numbers)
