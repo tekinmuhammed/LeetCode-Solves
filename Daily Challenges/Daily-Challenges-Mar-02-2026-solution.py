b8949f07834e72637ba5424cf47ba32b84ac135f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-02 18:12:22 +0300

    D-1536

--- Daily Challenges/Daily-Challenges-Mar-02-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-02-2026-solution.py
@@ -5,3 +5,47 @@
 
 # 🧠 Problem Description
 # [Github LeetCode 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1689.%20Partitioning%20Into%20Minimum%20Number%20Of%20Deci-Binary%20Numbers)
+
+class Solution(object):
+    def minSwaps(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n = len(grid)
+        # Her satır için sondaki ardışık sıfır sayısını hesapla
+        trailing_zeros = []
+        for row in grid:
+            count = 0
+            for i in range(n - 1, -1, -1):
+                if row[i] == 0:
+                    count += 1
+                else:
+                    break
+            trailing_zeros.append(count)
+        
+        res = 0
+        for i in range(n):
+            # i. satır için gereken minimum sıfır sayısı n - 1 - i
+            target = n - 1 - i
+            
+            # Bu koşulu sağlayan en yakın satırı bul
+            found = -1
+            for j in range(i, n):
+                if trailing_zeros[j] >= target:
+                    found = j
+                    break
+            
+            # Eğer uygun satır yoksa imkansızdır
+            if found == -1:
+                return -1
+            
+            # Bulunan satırı i. pozisyona kadar kaydır (swap simülasyonu)
+            # j'den i'ye kadar olan her bir adım bir swap işlemidir
+            res += (found - i)
+            
+            # Listede satırların yerini kaydırarak güncelle
+            val = trailing_zeros.pop(found)
+            trailing_zeros.insert(i, val)
+            
+        return res
