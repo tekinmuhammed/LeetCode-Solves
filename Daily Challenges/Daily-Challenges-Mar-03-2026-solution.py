9f44b769c2b4b362d0eba286caffd56cc83c8527
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-03 14:43:04 +0300

    D-1545-2

--- Daily Challenges/Daily-Challenges-Mar-03-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-03-2026-solution.py
@@ -1,10 +1,10 @@
 # 1545. Find Kth Bit in Nth Binary String
 
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 1545](https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/description/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 1545](https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 1536. Minimum Swaps to Arrange a Binary Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1536.%20Minimum%20Swaps%20to%20Arrange%20a%20Binary%20Grid)
+# [Github LeetCode 1545. Find Kth Bit in Nth Binary String](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1545.%20Find%20Kth%20Bit%20in%20Nth%20Binary%20String-2)
 
 class Solution(object):
     def findKthBit(self, n, k):
@@ -12,17 +12,17 @@ class Solution(object):
             if n == 1:
                 return '0'
             
-            mid = 1 << (n - 1)   # 2^(n-1)
+            mid = 1 << (n - 1)   # 2^(n-1) 
             
             if k == mid:
                 return '1'
             elif k < mid:
                 return helper(n - 1, k)
             else:
-                # symmetric position
+                # symmetric position 
                 mirrored = mid - (k - mid)
                 bit = helper(n - 1, mirrored)
-                # invert
+                # invert 
                 return '1' if bit == '0' else '0'
         
         return helper(n, k)
