36e01f9032ec2ea8a046abe18543fc14b2a282d3
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-03 14:44:08 +0300

    D-1545-2

--- Daily Challenges/Daily-Challenges-Mar-03-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-03-2026-solution.py
@@ -1,6 +1,6 @@
-# 1545. Find Kth Bit in Nth Binary String
+# 1545. Find Kth Bit in Nth Binary String 
 
-# **Difficulty:** Medium
+# **Difficulty:** Medium 
 # **Problem Link:** [LeetCode 1545](https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/description/)
 
 # 🧠 Problem Description
@@ -12,17 +12,17 @@ class Solution(object):
             if n == 1:
                 return '0'
             
-            mid = 1 << (n - 1)   # 2^(n-1) 
+            mid = 1 << (n - 1)   # 2^(n-1)
             
             if k == mid:
                 return '1'
             elif k < mid:
                 return helper(n - 1, k)
             else:
-                # symmetric position 
+                # symmetric position
                 mirrored = mid - (k - mid)
                 bit = helper(n - 1, mirrored)
-                # invert 
+                # invert
                 return '1' if bit == '0' else '0'
         
         return helper(n, k)
