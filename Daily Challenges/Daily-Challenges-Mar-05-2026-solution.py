77a77b44c565001afa2433570b104d55950155b8
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-05 14:41:00 +0300

    D-1758

--- Daily Challenges/Daily-Challenges-Mar-05-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-05-2026-solution.py
@@ -1,10 +1,11 @@
 # 1758. Minimum Changes To Make Alternating Binary String
 
+# **Difficulty:** Easy  
+# **Problem Link:** [LeetCode 1758](https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/description/)
+
 # 🧠 Problem Description
 # [Github LeetCode 1582. Special Positions in a Binary Matrix](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1582.%20Special%20Positions%20in%20a%20Binary%20Matrix)
 
-**Difficulty:** Easy  
-**Problem Link:** [LeetCode 1758](https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/description/)
 class Solution(object):
     def minOperations(self, s):
         change1 = 0  # pattern starting with '0' -> 010101...
