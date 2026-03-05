327afcfc04cac1716fe90b979c753e36c453bb64
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-05 14:36:52 +0300

    D-1758

---
+++ Daily Challenges/Daily-Challenges-Mar-05-2026-solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minOperations(self, s):
+        change1 = 0  # pattern starting with '0' -> 010101...
+        change2 = 0  # pattern starting with '1' -> 101010...
+        
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] != '0':
+                    change1 += 1
+                if s[i] != '1':
+                    change2 += 1
+            else:
+                if s[i] != '1':
+                    change1 += 1
+                if s[i] != '0':
+                    change2 += 1
+        
+        return min(change1, change2)
