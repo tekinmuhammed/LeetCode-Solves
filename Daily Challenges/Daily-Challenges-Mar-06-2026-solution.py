ef0343e215fecfc3e3ebb99d65518fdc99e7ed33
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-06 23:56:48 +0300

    D-1784

---
+++ Daily Challenges/Daily-Challenges-Mar-06-2026-solution.py
@@ -0,0 +1,4 @@
+# 1784. Check if Binary String Has at Most One Segment of Ones
+
+**Difficulty:** Easy  
+**Problem Link:** [LeetCode 1784](https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/)
