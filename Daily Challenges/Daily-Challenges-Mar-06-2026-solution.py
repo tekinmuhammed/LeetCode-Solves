e6c6084b82e17c595b3b9f3b2d3c265d52b61025
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-06 23:57:20 +0300

    D-1784

--- Daily Challenges/Daily-Challenges-Mar-06-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-06-2026-solution.py
@@ -2,3 +2,6 @@
 
 **Difficulty:** Easy  
 **Problem Link:** [LeetCode 1784](https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1582. Special Positions in a Binary Matrix](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1784.%20Check%20if%20Binary%20String%20Has%20at%20Most%20One%20Segment%20of%20Ones)
