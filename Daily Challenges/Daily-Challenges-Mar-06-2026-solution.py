1b880c1c7a1362912b8f474ad56e1671362641ed
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-06 23:57:47 +0300

    D-1784

--- Daily Challenges/Daily-Challenges-Mar-06-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-06-2026-solution.py
@@ -1,7 +1,11 @@
 # 1784. Check if Binary String Has at Most One Segment of Ones
 
-**Difficulty:** Easy  
-**Problem Link:** [LeetCode 1784](https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/)
+# **Difficulty:** Easy  
+# **Problem Link:** [LeetCode 1784](https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/)
 
 # 🧠 Problem Description 
-# [Github LeetCode 1582. Special Positions in a Binary Matrix](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1784.%20Check%20if%20Binary%20String%20Has%20at%20Most%20One%20Segment%20of%20Ones)
+# [Github LeetCode 1784. Check if Binary String Has at Most One Segment of Ones](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1784.%20Check%20if%20Binary%20String%20Has%20at%20Most%20One%20Segment%20of%20Ones)
+
+class Solution(object):
+    def checkOnesSegment(self, s):
+        return "01" not in s
