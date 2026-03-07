aef1b77a998201b73a3d18ebfcc7ed1c03b4b0b4
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-07 16:24:20 +0300

    D-1888

--- Daily Challenges/Daily-Challenges-Mar-07-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-07-2026-solution.py
@@ -1,7 +1,41 @@
 # 1888. Minimum Number of Flips to Make the Binary String Alternating
 
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 1888](https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/)
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 1888](https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/)
 
 # 🧠 Problem Description 
-# [Github LeetCode 1784. Check if Binary String Has at Most One Segment of Ones](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1784.%20Check%20if%20Binary%20String%20Has%20at%20Most%20One%20Segment%20of%20Ones)
+# [Github LeetCode 1888. Minimum Number of Flips to Make the Binary String Alternating](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1888.%20Minimum%20Number%20of%20Flips%20to%20Make%20the%20Binary%20String%20Alternating)
+
+class Solution(object):
+    def minFlips(self, s):
+        n = len(s)
+        s = s + s
+        
+        alt1 = ""
+        alt2 = ""
+        
+        for i in range(2 * n):
+            alt1 += "0" if i % 2 == 0 else "1"
+            alt2 += "1" if i % 2 == 0 else "0"
+        
+        diff1 = diff2 = 0
+        res = float('inf')
+        left = 0
+        
+        for right in range(2 * n):
+            if s[right] != alt1[right]:
+                diff1 += 1
+            if s[right] != alt2[right]:
+                diff2 += 1
+            
+            if right - left + 1 > n:
+                if s[left] != alt1[left]:
+                    diff1 -= 1
+                if s[left] != alt2[left]:
+                    diff2 -= 1
+                left += 1
+            
+            if right - left + 1 == n:
+                res = min(res, diff1, diff2)
+        
+        return res
