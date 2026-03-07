0fa6c8133323c9e4dd8db8374f258550d0207908
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-07 16:23:22 +0300

    D-1888

--- Daily Challenges/Daily-Challenges-Mar-07-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-07-2026-solution.py
@@ -2,3 +2,6 @@
 
 **Difficulty:** Medium  
 **Problem Link:** [LeetCode 1888](https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1784. Check if Binary String Has at Most One Segment of Ones](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1784.%20Check%20if%20Binary%20String%20Has%20at%20Most%20One%20Segment%20of%20Ones)
