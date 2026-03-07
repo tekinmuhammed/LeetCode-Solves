6ceba1bc42d81e46e3cff25a31443cc2b8078b40
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-07 16:22:45 +0300

    D-1888

---
+++ Daily Challenges/Daily-Challenges-Mar-07-2026-solution.py
@@ -0,0 +1,4 @@
+# 1888. Minimum Number of Flips to Make the Binary String Alternating
+
+**Difficulty:** Medium  
+**Problem Link:** [LeetCode 1888](https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/)
