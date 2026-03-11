f52a6979cb8223502e7069fc5f130515b8b37ab2
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-11 16:12:42 +0300

    D-1009

--- Daily Challenges/Daily-Challenges-Mar-11-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-11-2026-solution.py
@@ -1,7 +1,21 @@
 # 1009. Complement of Base 10 Integer
 
-**Difficulty:** Easy 
-**Problem Link:** [LeetCode 1009](https://leetcode.com/problems/complement-of-base-10-integer/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 1009](https://leetcode.com/problems/complement-of-base-10-integer/)
 
 # 🧠 Problem Description
-# [Github LeetCode 3130. Find All Possible Stable Binary Arrays II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/3130.%20Find%20All%20Possible%20Stable%20Binary%20Arrays%20II)
+# [Github LeetCode 1009. Complement of Base 10 Integer](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1009.%20Complement%20of%20Base%2010%20Integer)
+
+class Solution(object):
+    def bitwiseComplement(self, n):
+        if n == 0:
+            return 1
+        
+        mask = 0
+        temp = n
+        
+        while temp > 0:
+            mask = (mask << 1) | 1
+            temp >>= 1
+        
+        return n ^ mask
