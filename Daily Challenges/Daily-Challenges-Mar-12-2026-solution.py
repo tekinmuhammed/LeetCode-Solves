62ab106e41784e11b05093454d268b7348ff3c04
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-12 17:25:02 +0300

    D-3600

---
+++ Daily Challenges/Daily-Challenges-Mar-12-2026-solution.py
@@ -0,0 +1,7 @@
+# 3600. Maximize Spanning Tree Stability with Upgrades
+
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 3600](https://leetcode.com/problems/maximize-spanning-tree-stability-with-upgrades/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3600. Maximize Spanning Tree Stability with Upgrades](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/3600.%20Maximize%20Spanning%20Tree%20Stability%20with%20Upgrades)
