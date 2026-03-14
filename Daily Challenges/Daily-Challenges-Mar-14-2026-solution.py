937e640f4338d1e8bf443ca7a6cc516c9e925751
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-14 18:27:08 +0300

    D-1415

--- Daily Challenges/Daily-Challenges-Mar-14-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-14-2026-solution.py
@@ -1,10 +1,10 @@
-# 1415. The k-th Lexicographical String of All Happy Strings of Length n
-
-**Difficulty:** Medium
-**Problem Link:** [LeetCode 1415](https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/)
+# 1415. The k-th Lexicographical String of All Happy Strings of Length n 
  
-# 🧠 Problem Description
-# [Github LeetCode 3296. Minimum Number of Seconds to Make Mountain Height Zero](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1415.%20The%20k-th%20Lexicographical%20String%20of%20All%20Happy%20Strings%20of%20Length%20n)
+# **Difficulty:** Medium  
+# **Problem Link:** [LeetCode 1415](https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/)
+  
+# 🧠 Problem Description  
+# [Github LeetCode 3296. Minimum Number of Seconds to Make Mountain Height Zero](1415. The k-th Lexicographical String of All Happy Strings of Length n)
 
 class Solution(object):
     def getHappyString(self, n, k):
