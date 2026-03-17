1bcaa38e4f0146bae4e32828e913f3634888ad19
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-17 16:15:47 +0300

    D-1727

--- Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
@@ -1,16 +1,16 @@
-# 1727. Largest Submatrix With Rearrangements
-
-# **Difficulty:** Medium
-# **Problem Link:** [LeetCode 1727](https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/)
-
+# 1727. Largest Submatrix With Rearrangements 
+ 
+# **Difficulty:** Medium 
+# **Problem Link:** [LeetCode 1727](https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/) 
+ 
 # 🧠 Problem Description
-# [Github LeetCode 1727. Largest Submatrix With Rearrangements](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1727.%20Largest%20Submatrix%20With%20Rearrangements)
-
+# [Github LeetCode 1727. Largest Submatrix With Rearrangements](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1727.%20Largest%20Submatrix%20With%20Rearrangements) 
+ 
 class Solution(object):
     def largestSubmatrix(self, matrix):
         m, n = len(matrix), len(matrix[0])
         
-        # build heights
+        # build heights 
         for i in range(1, m):
             for j in range(n):
                 if matrix[i][j] == 1:
