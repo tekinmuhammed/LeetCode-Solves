4490056917abe4800a1281a29458b42dbe41ca99
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-17 16:09:11 +0300

    D-1727

--- Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
@@ -1,7 +1,27 @@
-# 1727. Largest Submatrix With Rearrangements 
-
-**Difficulty:** Medium  
+# 1727. Largest Submatrix With Rearrangements  
+ 
+**Difficulty:** Medium   
 **Problem Link:** [LeetCode 1727](https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/)
-
-# 🧠 Problem Description 
-# [Github LeetCode 1878. Get Biggest Three Rhombus Sums in a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1878.%20Get%20Biggest%20Three%20Rhombus%20Sums%20in%20a%20Grid)
+ 
+# 🧠 Problem Description  
+# [Github LeetCode 1878. Get Biggest Three Rhombus Sums in a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1727.%20Largest%20Submatrix%20With%20Rearrangements)
+ 
+class Solution(object):
+    def largestSubmatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        
+        # build heights
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i-1][j]
+        
+        res = 0
+        
+        for i in range(m):
+            row = sorted(matrix[i], reverse=True)
+            
+            for j in range(n):
+                res = max(res, row[j] * (j + 1))
+        
+        return res
