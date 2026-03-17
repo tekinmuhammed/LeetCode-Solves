abb4f9c9128563c2aeccfd99e7a51fe09645210a
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-17 16:07:48 +0300

    D-1727

---
+++ Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
@@ -0,0 +1,4 @@
+# 1727. Largest Submatrix With Rearrangements
+ 
+**Difficulty:** Medium 
+**Problem Link:** [LeetCode 1727](https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/)
