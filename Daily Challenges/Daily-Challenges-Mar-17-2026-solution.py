ef408d591b6b91b9a7232436696e9ffa5c31f87f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-17 16:08:24 +0300

    D-1727

--- Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-17-2026-solution.py
@@ -1,4 +1,7 @@
-# 1727. Largest Submatrix With Rearrangements
- 
-**Difficulty:** Medium 
+# 1727. Largest Submatrix With Rearrangements 
+
+**Difficulty:** Medium  
 **Problem Link:** [LeetCode 1727](https://leetcode.com/problems/largest-submatrix-with-rearrangements/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1878. Get Biggest Three Rhombus Sums in a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1878.%20Get%20Biggest%20Three%20Rhombus%20Sums%20in%20a%20Grid)
