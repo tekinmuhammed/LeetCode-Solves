195092e76c0d956640274711825cd86141b97446
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-18 16:55:44 +0300

    D-3070

--- Daily Challenges/Daily-Challenges-Mar-18-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-18-2026-solution.py
@@ -1,8 +1,30 @@
-# 3070. Count Submatrices with Top-Left Element and Sum Less Than k    
- 
-# **Difficulty:** Medium    
+# 3070. Count Submatrices with Top-Left Element and Sum Less Than k 
+
+# **Difficulty:** Medium
 # **Problem Link:** [LeetCode 3070](https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/)
- 
-# 🧠 Problem Description 
+
+# 🧠 Problem Description
 # [Github LeetCode 3070. Count Submatrices with Top-Left Element and Sum Less Than k](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3070.%20Count%20Submatrices%20with%20Top-Left%20Element%20and%20Sum%20Less%20Than%20k) 
- 
+
+class Solution(object):
+    def countSubmatrices(self, grid, k):
+        m, n = len(grid), len(grid[0])
+        
+        # prefix sum in-place
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    grid[i][j] += grid[i-1][j]
+                if j > 0:
+                    grid[i][j] += grid[i][j-1]
+                if i > 0 and j > 0:
+                    grid[i][j] -= grid[i-1][j-1]
+        
+        res = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] <= k:
+                    res += 1
+        
+        return res
