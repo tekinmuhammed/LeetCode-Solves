4566ed21a217c04dcc2c93570b31fd358c1f289b
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-18 16:54:57 +0300

    D-3070

--- Daily Challenges/Daily-Challenges-Mar-18-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-18-2026-solution.py
@@ -1,8 +1,8 @@
- # 3070. Count Submatrices with Top-Left Element and Sum Less Than k 
-
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 3070](https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/)
-
-# 🧠 Problem Description
-# [Github LeetCode 1727. Largest Submatrix With Rearrangements](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1727.%20Largest%20Submatrix%20With%20Rearrangements) 
+# 3070. Count Submatrices with Top-Left Element and Sum Less Than k    
+ 
+# **Difficulty:** Medium    
+# **Problem Link:** [LeetCode 3070](https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/)
+ 
+# 🧠 Problem Description 
+# [Github LeetCode 3070. Count Submatrices with Top-Left Element and Sum Less Than k](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3070.%20Count%20Submatrices%20with%20Top-Left%20Element%20and%20Sum%20Less%20Than%20k) 
  
