6223ef1bb175dca282f31f4dbc5c4b44c5fa7331
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-19 14:35:06 +0300

    D-3212

--- Daily Challenges/Daily-Challenges-Mar-19-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-19-2026-solution.py
@@ -1,7 +1,9 @@
-# 3212. Count Submatrices With Equal Frequency of X and Y
+# 3212. Count Submatrices With Equal Frequency of X and Y 
+ 
+# **Diffic ulty:** Medium   
+# **Problem Link:** [LeetCode 3212](https://leetcode.com/problems/count-submatrices-with-equal-frequency-of-x-and-y/)
+ 
+# 🧠 Problem Description 
+# [Github LeetCode 3070. Count Submatrices with Top-Left Element and Sum Less Than k](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3212.%20Count%20Submatrices%20With%20Equal%20Frequency%20of%20X%20and%20Y) 
 
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 3212](https://leetcode.com/problems/count-submatrices-with-equal-frequency-of-x-and-y/)
 
-# 🧠 Problem Description
-# [Github LeetCode 3070. Count Submatrices with Top-Left Element and Sum Less Than k](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3070.%20Count%20Submatrices%20with%20Top-Left%20Element%20and%20Sum%20Less%20Than%20k) 
