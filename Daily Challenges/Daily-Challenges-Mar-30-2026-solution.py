0b18a1bf0315e8d520f2838ccef417368380532d
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-03-30 20:27:09 +0300

    D-2840

--- Daily Challenges/Daily-Challenges-Mar-30-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Mar-30-2026-solution.py
@@ -1,17 +1,11 @@
-
-
-
-
-
-
-
-
-
-
 # 2840. Check if Strings Can be Made Equal With Operations II
 
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 2840](https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 2840](https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2840. Check if Strings Can be Made Equal With Operations II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2840.%20Check%20if%20Strings%20Can%20be%20Made%20Equal%20With%20Operations%20II) 
+
 class Solution(object):
     def checkStrings(self, s1, s2):
         return (
