58cde270a540c7ae2da86479bf100ad962c077bf
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-03 15:30:12 +0300

    D-3661

---
+++ Daily Challenges/Daily-Challenges-Apr-03-2026-solution.py
@@ -0,0 +1,68 @@
+# 3661. Maximum Walls Destroyed by Robots
+
+**Difficulty:** Hard  
+**Problem Link:** [LeetCode 3661](https://leetcode.com/problems/maximum-walls-destroyed-by-robots/description/)
+ 
+# 🧠 Problem Description
+# [Github LeetCode 3418. Maximum Amount of Money Robot Can Earn](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3418.%20Maximum%20Amount%20of%20Money%20Robot%20Can%20Earn) 
+
+class Solution:
+    def maxWalls(
+        self, robots: List[int], distance: List[int], walls: List[int]
+    ) -> int:
+        n = len(robots)
+        left = [0] * n
+        right = [0] * n
+        num = [0] * n
+        robots_to_distance = {}
+
+        for i in range(n):
+            robots_to_distance[robots[i]] = distance[i]
+
+        robots.sort()
+        walls.sort()
+
+        for i in range(n):
+            pos1 = bisect.bisect_right(walls, robots[i])
+
+            if i >= 1:
+                left_bound = max(
+                    robots[i] - robots_to_distance[robots[i]], robots[i - 1] + 1
+                )
+                left_pos = bisect.bisect_left(walls, left_bound)
+            else:
+                left_pos = bisect.bisect_left(
+                    walls, robots[i] - robots_to_distance[robots[i]]
+                )
+
+            left[i] = pos1 - left_pos
+
+            if i < n - 1:
+                right_bound = min(
+                    robots[i] + robots_to_distance[robots[i]], robots[i + 1] - 1
+                )
+                right_pos = bisect.bisect_right(walls, right_bound)
+            else:
+                right_pos = bisect.bisect_right(
+                    walls, robots[i] + robots_to_distance[robots[i]]
+                )
+
+            pos2 = bisect.bisect_left(walls, robots[i])
+            right[i] = right_pos - pos2
+
+            if i == 0:
+                continue
+
+            pos3 = bisect.bisect_left(walls, robots[i - 1])
+            num[i] = pos1 - pos3
+
+        sub_left, sub_right = left[0], right[0]
+        for i in range(1, n):
+            current_left = max(
+                sub_left + left[i],
+                sub_right - right[i - 1] + min(left[i] + right[i - 1], num[i]),
+            )
+            current_right = max(sub_left + right[i], sub_right + right[i])
+            sub_left, sub_right = current_left, current_right
+
+        return max(sub_left, sub_right)
