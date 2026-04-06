e240c8d8939c909a0982f38afbd06dfda41c3427
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-06 12:39:07 +0300

    D-874

---
+++ Daily Challenges/Daily-Challenges-Apr-06-2026-solution.py
@@ -0,0 +1,7 @@
+# 874. Walking Robot Simulation
+
+**Difficulty:** Medium  
+**Problem Link:** [LeetCode 874](https://leetcode.com/problems/walking-robot-simulation/)
+
+# 🧠 Problem Description
+# [Github LeetCode 657. Robot Return to Origin](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/657.%20Robot%20Return%20to%20Origin) 
