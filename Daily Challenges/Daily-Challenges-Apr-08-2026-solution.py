824a2af795a6ec254436ec043b118ea9861f5eca
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-08 15:42:53 +0300

    D-3653

--- Daily Challenges/Daily-Challenges-Apr-08-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-08-2026-solution.py
@@ -1,7 +1,21 @@
 # 3653. XOR After Range Multiplication Queries I
 
-# 🧠 Problem Description
-# [Github LeetCode 2069. Walking Robot Simulation II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2069.%20Walking%20Robot%20Simulation%20II) 
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 3653](https://leetcode.com/problems/xor-after-range-multiplication-queries-i/)
 
-**Difficulty:** Medium  
-**Problem Link:** [LeetCode 3653](https://leetcode.com/problems/xor-after-range-multiplication-queries-i/)
+# 🧠 Problem Description
+# [Github LeetCode 3653. XOR After Range Multiplication Queries I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3653.%20XOR%20After%20Range%20Multiplication%20Queries%20I) 
+
+class Solution:
+    MOD = 10**9 + 7
+
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        for l, r, k, v in queries:
+            for i in range(l, r + 1, k):
+                nums[i] = (nums[i] * v) % self.MOD
+
+        res = 0
+        for x in nums:
+            res ^= x
+
+        return res
