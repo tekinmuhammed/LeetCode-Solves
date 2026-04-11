673989655dc01f81a252bf0085a72ed4c79e23e2
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-11 16:45:45 +0300

    D-3741

--- Daily Challenges/Daily-Challenges-Apr-11-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-11-2026-solution.py
@@ -1,1 +1,7 @@
-.
+# 3741. Minimum Distance Between Three Equal Elements II
+
+**Difficulty:** Medium  
+**Problem Link:** [LeetCode 3741](https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3740. Minimum Distance Between Three Equal Elements I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3740.%20Minimum%20Distance%20Between%20Three%20Equal%20Elements%20I) 
