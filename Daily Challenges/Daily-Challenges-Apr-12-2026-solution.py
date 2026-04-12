32aa78f67b301b2626edbebaa904129305644e06
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-12 16:35:19 +0300

    D-1320

--- Daily Challenges/Daily-Challenges-Apr-12-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-12-2026-solution.py
@@ -1,10 +1,10 @@
 # 1320. Minimum Distance to Type a Word Using Two Fingers
 
-**Difficulty:** Hard
-**Problem Link:** [LeetCode 1320](https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/description/)
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 1320](https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 3655. XOR After Range Multiplication Queries II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3655.%20XOR%20After%20Range%20Multiplication%20Queries%20II) 
+# [Github LeetCode 1320. Minimum Distance to Type a Word Using Two Fingers](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1320.%20Minimum%20Distance%20to%20Type%20a%20Word%20Using%20Two%20Fingers) 
 
 class Solution:
     def minimumDistance(self, word: str) -> int:
