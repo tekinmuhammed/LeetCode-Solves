4a88136cb1b6340fcc98680c383816bcb70cb7d6
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-13 13:40:39 +0300

    D-1848

--- Daily Challenges/Daily-Challenges-Apr-13-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-13-2026-solution.py
@@ -1,3 +1,11 @@
+# 1848. Minimum Distance to the Target Element
+
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 1848](https://leetcode.com/problems/minimum-distance-to-the-target-element/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1848. Minimum Distance to the Target Element](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1848.%20Minimum%20Distance%20to%20the%20Target%20Element) 
+
 class Solution:
     def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
         res = len(nums)
