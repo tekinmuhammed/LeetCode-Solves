9a1d100d3fb9cc97f8ac477dce291b7c06e9a517
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-14 18:03:15 +0300

    D-2463

---
+++ Daily Challenges/Daily-Challenges-Apr-14-2026-solution.py
@@ -0,0 +1,7 @@
+# 2463. Minimum Total Distance Traveled
+
+**Difficulty:** Hard  
+**Problem Link:** [LeetCode 2463](https://leetcode.com/problems/minimum-total-distance-traveled/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1848. Minimum Distance to the Target Element](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1848.%20Minimum%20Distance%20to%20the%20Target%20Element) 
