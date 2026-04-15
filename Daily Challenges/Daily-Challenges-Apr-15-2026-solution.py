55055fc40ea712c43bfe34199836d855d896d75e
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-15 17:58:57 +0300

    D-2515

--- Daily Challenges/Daily-Challenges-Apr-15-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-15-2026-solution.py
@@ -1,8 +1,17 @@
 # 2515. Shortest Distance to Target String in a Circular Array
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 2515](https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/)
-
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 2515](https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 2463. Minimum Total Distance Traveled](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/2463.%20Minimum%20Total%20Distance%20Traveled) 
+# [Github LeetCode 2515. Shortest Distance to Target String in a Circular Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2515.%20Shortest%20Distance%20to%20Target%20String%20in%20a%20Circular%20Array) 
+
+class Solution:
+    def closestTarget(
+        self, words: List[str], target: str, startIndex: int
+    ) -> int:
+        ans = n = len(words)
+        for i, word in enumerate(words):
+            if word == target:
+                ans = min(ans, abs(i - startIndex), n - abs(i - startIndex))
+        return ans if ans < n else -1
