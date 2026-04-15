52fdeef902f345f9b7c1a087c6d94c4785f1f63f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-15 17:58:07 +0300

    D-2515

---
+++ Daily Challenges/Daily-Challenges-Apr-15-2026-solution.py
@@ -0,0 +1,8 @@
+# 2515. Shortest Distance to Target String in a Circular Array
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 2515](https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/)
+
+
+# 🧠 Problem Description
+# [Github LeetCode 2463. Minimum Total Distance Traveled](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/2463.%20Minimum%20Total%20Distance%20Traveled) 
