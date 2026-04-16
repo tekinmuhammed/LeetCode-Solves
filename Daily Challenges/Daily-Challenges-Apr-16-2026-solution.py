a1a2fe80a79db37e866bdd60758d674d6b04ecb6
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-16 15:42:16 +0300

    D-3488

---
+++ Daily Challenges/Daily-Challenges-Apr-16-2026-solution.py
@@ -0,0 +1,7 @@
+# 3488. Closest Equal Element Queries
+
+**Difficulty:** Medium
+**Problem Link:** [LeetCode 3488](https://leetcode.com/problems/closest-equal-element-queries/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2515. Shortest Distance to Target String in a Circular Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2515.%20Shortest%20Distance%20to%20Target%20String%20in%20a%20Circular%20Array) 
