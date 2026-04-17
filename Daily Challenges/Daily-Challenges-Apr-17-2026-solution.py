7b4f55f739feea336e3e0fcee6284150faa84bdb
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-17 13:29:07 +0300

    D-3761

--- Daily Challenges/Daily-Challenges-Apr-17-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-17-2026-solution.py
@@ -1,3 +1,11 @@
+# 3761. Minimum Absolute Distance Between Mirror Pairs
+
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 3761](https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3761. Minimum Absolute Distance Between Mirror Pairs](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3761.%20Minimum%20Absolute%20Distance%20Between%20Mirror%20Pairs) 
+
 class Solution:
     def minMirrorPairDistance(self, nums: List[int]) -> int:
         prev = dict()
@@ -7,13 +15,3 @@ class Solution:
                 ans = min(ans, i - prev[num])
             prev[int(str(num)[::-1])] = i
         return -1 if ans == inf else ans
-    
-
-    # 3761. Minimum Absolute Distance Between Mirror Pairs
-
-**Difficulty:** Medium
-**Problem Link:** [LeetCode 3761](https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/description/)
-
-
-# 🧠 Problem Description
-# [Github LeetCode 3488. Closest Equal Element Queries](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3488.%20Closest%20Equal%20Element%20Queries) 
