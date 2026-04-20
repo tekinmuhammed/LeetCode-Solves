43293b8fa4875d9d88f01b2c40a708907eda6a4b
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-20 17:22:09 +0300

    D-2078

--- Daily Challenges/Daily-Challenges-Apr-20-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-20-2026-solution.py
@@ -1,10 +1,10 @@
 # 2078. Two Furthest Houses With Different Colors
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 2078](https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 2078](https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 1855. Maximum Distance Between a Pair of Values](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1855.%20Maximum%20Distance%20Between%20a%20Pair%20of%20Values) 
+# [Github LeetCode 2078. Two Furthest Houses With Different Colors](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2078.%20Two%20Furthest%20Houses%20With%20Different%20Colors) 
 
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
