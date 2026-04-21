f672a9f149465fa24f241ddc1c20b933fd01db5b
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-21 18:19:45 +0300

    D-1722

---
+++ Daily Challenges/Daily-Challenges-Apr-21-2026-solution.py
@@ -0,0 +1,7 @@
+# 1722. Minimize Hamming Distance After Swap Operations
+
+**Difficulty:** Medium
+**Problem Link:** [LeetCode 1722](https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2078. Two Furthest Houses With Different Colors](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2078.%20Two%20Furthest%20Houses%20With%20Different%20Colors) 
