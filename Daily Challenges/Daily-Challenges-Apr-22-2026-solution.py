08c6be0b4c3394c97ba533756ab4b57c9d18d6f2
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-22 14:43:09 +0300

    D-2452

--- Daily Challenges/Daily-Challenges-Apr-22-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-22-2026-solution.py
@@ -3,6 +3,9 @@
 **Difficulty:** Medium
 **Problem Link:** [LeetCode 2452](https://leetcode.com/problems/words-within-two-edits-of-dictionary/description/)
 
+# 🧠 Problem Description
+# [Github LeetCode 1722. Minimize Hamming Distance After Swap Operations](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1722.%20Minimize%20Hamming%20Distance%20After%20Swap%20Operations) 
+
 class Solution:
     def twoEditWords(self, queries, dictionary):
         ans = []
@@ -16,3 +19,5 @@ class Solution:
                     ans.append(query)
                     break
         return ans
+
+https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2452.%20Words%20Within%20Two%20Edits%20of%20Dictionary
