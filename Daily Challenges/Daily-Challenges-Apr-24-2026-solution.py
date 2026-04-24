fc033eef0717f607cf393e1cc4469d8e4b3ba0b1
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-24 14:27:51 +0300

    D-2833

--- Daily Challenges/Daily-Challenges-Apr-24-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Apr-24-2026-solution.py
@@ -1,7 +1,11 @@
 # 2833. Furthest Point From Origin 
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 2833](https://leetcode.com/problems/furthest-point-from-origin/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 2833](https://leetcode.com/problems/furthest-point-from-origin/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 2615. Sum of Distances](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2615.%20Sum%20of%20Distances) 
+# [Github LeetCode 2833. Furthest Point From Origin ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2833.%20Furthest%20Point%20From%20Origin) 
+
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        return abs(moves.count("R") - moves.count("L")) + moves.count("_")
