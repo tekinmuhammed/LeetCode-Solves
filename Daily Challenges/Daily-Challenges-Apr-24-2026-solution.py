a9e843f10742bee195c81cda60b0dc08a7912374
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-24 14:27:20 +0300

    D-2833

---
+++ Daily Challenges/Daily-Challenges-Apr-24-2026-solution.py
@@ -0,0 +1,7 @@
+# 2833. Furthest Point From Origin 
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 2833](https://leetcode.com/problems/furthest-point-from-origin/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2615. Sum of Distances](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2615.%20Sum%20of%20Distances) 
