357d53ec4207d0584a178b399cde8949abc749e2
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-29 13:43:58 +0300

    D-3225

---
+++ Daily Challenges/Daily-Challenges-Apr-29-2026-solution.py
@@ -0,0 +1,7 @@
+# 3225. Maximum Score From Grid Operations
+
+**Difficulty:** Hard  
+**Problem Link:** [LeetCode 3225](https://leetcode.com/problems/maximum-score-from-grid-operations/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2033. Minimum Operations to Make a Uni-Value Grid ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2033.%20Minimum%20Operations%20to%20Make%20a%20Uni-Value%20Grid) 
