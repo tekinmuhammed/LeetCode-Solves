f9d5ee89a1801a390ea5bc5aca9c2a8bda8d53e9
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-04-30 15:58:54 +0300

    D-3742

---
+++ Daily Challenges/Daily-Challenges-Apr-30-2026-solution.py
@@ -0,0 +1,7 @@
+# 3742. Maximum Path Score in a Grid 
+
+**Difficulty:** Medium
+**Problem Link:** [LeetCode 3742](https://leetcode.com/problems/maximum-path-score-in-a-grid/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3225. Maximum Score From Grid Operations](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/3225.%20Maximum%20Score%20From%20Grid%20Operations) 
