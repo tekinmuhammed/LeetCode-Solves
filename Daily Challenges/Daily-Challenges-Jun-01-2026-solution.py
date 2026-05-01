457010b693130a986a58f0bffa1699fc44896661
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-01 12:03:43 +0300

    D-396

--- Daily Challenges/Daily-Challenges-Jun-01-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-01-2026-solution.py
@@ -1,7 +1,18 @@
 # 396. Rotate Function 
 
-**Difficulty:** Medium
-**Problem Link:** [LeetCode 396](https://leetcode.com/problems/rotate-function/description/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 396](https://leetcode.com/problems/rotate-function/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 3742. Maximum Path Score in a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3742.%20Maximum%20Path%20Score%20in%20a%20Grid) 
+# [Github LeetCode 396. Rotate Function](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/396.%20Rotate%20Function) 
+
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        f, n, numSum = 0, len(nums), sum(nums)
+        for i, num in enumerate(nums):
+            f += i * num
+        res = f
+        for i in range(n - 1, 0, -1):
+            f = f + numSum - n * nums[i]
+            res = max(res, f)
+        return res
