be1595d5ac167b9baec7009a509a0a136a239f0c
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-01 12:02:57 +0300

    D-396

---
+++ Daily Challenges/Daily-Challenges-Jun-01-2026-solution.py
@@ -0,0 +1,7 @@
+# 396. Rotate Function 
+
+**Difficulty:** Medium
+**Problem Link:** [LeetCode 396](https://leetcode.com/problems/rotate-function/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3742. Maximum Path Score in a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3742.%20Maximum%20Path%20Score%20in%20a%20Grid) 
