6e15626bdc981b9d22527998886611da2bf26eea
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-02 13:41:08 +0300

    D-788

---
+++ Daily Challenges/Daily-Challenges-Jun-02-2026-solution.py
@@ -0,0 +1,24 @@
+
+# 🧠 Problem Description
+# [Github LeetCode 396. Rotate Function](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/396.%20Rotate%20Function) 
+
+class Solution(object):
+    def rotatedDigits(self, n):
+        good = 0
+        
+        for num in range(1, n + 1):
+            s = str(num)
+            valid = True
+            changed = False
+            
+            for ch in s:
+                if ch in '347':
+                    valid = False
+                    break
+                if ch in '2569':
+                    changed = True
+            
+            if valid and changed:
+                good += 1
+        
+        return good
