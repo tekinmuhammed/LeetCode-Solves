804d9ca5923375ec77428879adac60b05983e2d0
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-03 15:14:19 +0300

    D-796

--- Daily Challenges/Daily-Challenges-Jun-03-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-03-2026-solution.py
@@ -1,10 +1,10 @@
 # 796. Rotate String 
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 796](https://leetcode.com/problems/rotate-string/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 796](https://leetcode.com/problems/rotate-string/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 788. Rotated Digits](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/788.%20Rotated%20Digits) 
+# [Github LeetCode 796. Rotate String](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/796.%20Rotate%20String) 
 
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
@@ -12,9 +12,9 @@ class Solution:
             return False
         length = len(s)
 
-        # Try all possible rotations of the string
+        # Try all possible rotations of the string 
         for _ in range(length):
-            # Perform one rotation
+            # Perform one rotation 
             s = s[1:] + s[0]
             if s == goal:
                 return True
