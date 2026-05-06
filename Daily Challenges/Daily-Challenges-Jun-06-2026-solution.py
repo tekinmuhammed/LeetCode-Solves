ec39aa8b70649c874bf22d63cea1278fce90ce77
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-06 15:27:09 +0300

    D-1861

---
+++ Daily Challenges/Daily-Challenges-Jun-06-2026-solution.py
@@ -0,0 +1,7 @@
+# 1861. Rotating the Box 
+
+**Difficulty:** Medium=
+**Problem Link:** [LeetCode 1861](https://leetcode.com/problems/rotating-the-box/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 61. Rotate List ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/61.%20Rotate%20List) 
