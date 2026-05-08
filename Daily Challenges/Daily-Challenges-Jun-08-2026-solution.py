aa4be08caaf8df9e932b0cde73d9b4cf038d7290
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-08 14:21:52 +0300

    D-3629

--- Daily Challenges/Daily-Challenges-Jun-08-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-08-2026-solution.py
@@ -1,7 +1,7 @@
 # 3629. Minimum Jumps to Reach End via Prime Teleportation
 
-**Difficulty:** Medium
-**Problem Link:** [LeetCode 3629](https://leetcode.com/problems/minimum-jumps-to-reach-end-via-prime-teleportation/description/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 3629](https://leetcode.com/problems/minimum-jumps-to-reach-end-via-prime-teleportation/description/)
 
 # 🧠 Problem Description 
-# [Github LeetCode 3660. Jump Game IX](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3660.%20Jump%20Game%20IX) 
+# [Github LeetCode 3629. Minimum Jumps to Reach End via Prime Teleportation](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3629.%20Minimum%20Jumps%20to%20Reach%20End%20via%20Prime%20Teleportation) 
