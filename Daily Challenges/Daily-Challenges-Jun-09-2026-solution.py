0f8ab2dc8c99aaa67f14c7ecb6749014392825be
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-09 18:04:48 +0300

    D-1914

---
+++ Daily Challenges/Daily-Challenges-Jun-09-2026-solution.py
@@ -0,0 +1,7 @@
+# 1914. Cyclically Rotating a Grid
+
+**Difficulty:** Medium 
+**Problem Link:** [LeetCode 1914](https://leetcode.com/problems/cyclically-rotating-a-grid/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 3629. Minimum Jumps to Reach End via Prime Teleportation](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3629.%20Minimum%20Jumps%20to%20Reach%20End%20via%20Prime%20Teleportation) 
