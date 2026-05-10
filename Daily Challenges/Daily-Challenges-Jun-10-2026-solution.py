840f168c294cd2c00686e966056579b7de0a3fe5
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-10 14:24:01 +0300

    D-2770

---
+++ Daily Challenges/Daily-Challenges-Jun-10-2026-solution.py
@@ -0,0 +1,7 @@
+# 2770. Maximum Number of Jumps to Reach the Last Index
+
+**Difficulty:** Medium
+**Problem Link:** [LeetCode 2770](https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1914. Cyclically Rotating a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1914.%20Cyclically%20Rotating%20a%20Grid) 
