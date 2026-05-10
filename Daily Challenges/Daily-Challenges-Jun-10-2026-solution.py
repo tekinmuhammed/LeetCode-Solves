6ff1d17a93ee498ccfc283984ea3edfbda4c2f9e
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-10 14:24:49 +0300

    D-2770

--- Daily Challenges/Daily-Challenges-Jun-10-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-10-2026-solution.py
@@ -1,7 +1,23 @@
 # 2770. Maximum Number of Jumps to Reach the Last Index
 
-**Difficulty:** Medium
-**Problem Link:** [LeetCode 2770](https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 2770](https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/)
 
-# 🧠 Problem Description 
-# [Github LeetCode 1914. Cyclically Rotating a Grid](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1914.%20Cyclically%20Rotating%20a%20Grid) 
+# 🧠 Problem Description
+# [Github LeetCode 2770. Maximum Number of Jumps to Reach the Last Index](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2770.%20Maximum%20Number%20of%20Jumps%20to%20Reach%20the%20Last%20Index) 
+
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        @cache
+        def dfs(i: int):
+            if i == len(nums) - 1:
+                return 0
+
+            res = -inf
+            for j in range(i + 1, len(nums)):
+                if abs(nums[i] - nums[j]) <= target:
+                    res = max(res, dfs(j) + 1)
+            return res
+
+        ans = dfs(0)
+        return -1 if ans < 0 else ans
