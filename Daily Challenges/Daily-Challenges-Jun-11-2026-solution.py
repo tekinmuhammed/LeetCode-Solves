6c5cd4c86e72cae38989aa8d48b7f4f588b505d9
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-11 14:06:26 +0300

    D-2553

---
+++ Daily Challenges/Daily-Challenges-Jun-11-2026-solution.py
@@ -0,0 +1,7 @@
+# 2553. Separate the Digits in an Array
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 2553](https://leetcode.com/problems/separate-the-digits-in-an-array/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 2770. Maximum Number of Jumps to Reach the Last Index](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2770.%20Maximum%20Number%20of%20Jumps%20to%20Reach%20the%20Last%20Index) 
