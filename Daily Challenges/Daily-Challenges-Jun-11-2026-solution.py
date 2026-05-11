0d8e5c3d01a38685437d42838e1f61e96364d659
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-11 14:07:36 +0300

    D-2553

--- Daily Challenges/Daily-Challenges-Jun-11-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-11-2026-solution.py
@@ -1,7 +1,18 @@
 # 2553. Separate the Digits in an Array
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 2553](https://leetcode.com/problems/separate-the-digits-in-an-array/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 2553](https://leetcode.com/problems/separate-the-digits-in-an-array/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 2770. Maximum Number of Jumps to Reach the Last Index](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/2770.%20Maximum%20Number%20of%20Jumps%20to%20Reach%20the%20Last%20Index) 
+# [Github LeetCode 2553. Separate the Digits in an Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2553.%20Separate%20the%20Digits%20in%20an%20Array) 
+
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        res = []
+        for x in nums:
+            tmp = []
+            while x > 0:
+                tmp.append(x % 10)
+                x //= 10
+            res.extend(tmp[::-1])
+        return res
