8ce69ae149a2966aaa9765af2f18e0b9248baabc
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-12 20:36:33 +0300

    D-1665

--- Daily Challenges/Daily-Challenges-Jun-12-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-12-2026-solution.py
@@ -1,3 +1,11 @@
+# 1665. Minimum Initial Energy to Finish Tasks
+
+# **Difficulty:** Hard  
+# **Problem Link:** [LeetCode 1665](https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1665. Minimum Initial Energy to Finish Tasks](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1665.%20Minimum%20Initial%20Energy%20to%20Finish%20Tasks) 
+
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
         tasks.sort(key=lambda x: x[1] - x[0])
