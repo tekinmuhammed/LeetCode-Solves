b81ec800085a16177a2aa7f715d6c8196ea2d650
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-14 13:10:29 +0300

    D-2784

---
+++ Daily Challenges/Daily-Challenges-Jun-14-2026-solution.py
@@ -0,0 +1,7 @@
+# 2784. Check if Array is Good
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 2784](https://leetcode.com/problems/check-if-array-is-good/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1674. Minimum Moves to Make Array Complementary ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1674.%20Minimum%20Moves%20to%20Make%20Array%20Complementary) 
