4d283b0ce1579955dcc07697fef71c890bd0ac14
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-14 13:11:05 +0300

    D-2784

--- Daily Challenges/Daily-Challenges-Jun-14-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-14-2026-solution.py
@@ -1,7 +1,21 @@
 # 2784. Check if Array is Good
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 2784](https://leetcode.com/problems/check-if-array-is-good/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 2784](https://leetcode.com/problems/check-if-array-is-good/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 1674. Minimum Moves to Make Array Complementary ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1674.%20Minimum%20Moves%20to%20Make%20Array%20Complementary) 
+# [Github LeetCode 2784. Check if Array is Good](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2784.%20Check%20if%20Array%20is%20Good) 
+
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = [0] * n
+        for a in nums:
+            if a >= n:
+                return False
+            if a < n - 1 and count[a] > 0:
+                return False
+            if a == n - 1 and count[a] > 1:
+                return False
+            count[a] += 1
+        return True
