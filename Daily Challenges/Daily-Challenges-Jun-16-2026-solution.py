e6cf9e74b7e0d4c31683b8cd48cf73ff9393ae41
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-16 22:33:17 +0300

    D-154

--- Daily Challenges/Daily-Challenges-Jun-16-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-16-2026-solution.py
@@ -1,7 +1,25 @@
 # 154. Find Minimum in Rotated Sorted Array II
 
-**Difficulty:** Hard
-**Problem Link:** [LeetCode 154](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/)
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 154](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 153. Find Minimum in Rotated Sorted Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/153.%20Find%20Minimum%20in%20Rotated%20Sorted%20Array) 
+# [Github LeetCode 154. Find Minimum in Rotated Sorted Array II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/154.%20Find%20Minimum%20in%20Rotated%20Sorted%20Array%20II) 
+
+class Solution(object):
+    def findMin(self, nums):
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            
+            elif nums[mid] < nums[right]:
+                right = mid
+            
+            else:
+                right -= 1
+        
+        return nums[left]
