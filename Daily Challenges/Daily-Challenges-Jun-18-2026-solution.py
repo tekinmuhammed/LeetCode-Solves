2f55389a82e8f2ab00274d4f4d2fc32c07500f63
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-18 13:15:58 +0300

    D-1345

---
+++ Daily Challenges/Daily-Challenges-Jun-18-2026-solution.py
@@ -0,0 +1,7 @@
+# 1345. Jump Game IV
+
+**Difficulty:** Hard
+**Problem Link:** [LeetCode 1345](https://leetcode.com/problems/jump-game-iv/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1306. Jump Game III](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1306.%20Jump%20Game%20III) 
