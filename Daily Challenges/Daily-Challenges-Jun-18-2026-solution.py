b8e1fa710550c8b102b5cae67e2a5790a931f67c
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-18 13:17:10 +0300

    D-1345

--- Daily Challenges/Daily-Challenges-Jun-18-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-18-2026-solution.py
@@ -1,7 +1,54 @@
 # 1345. Jump Game IV
 
-**Difficulty:** Hard
-**Problem Link:** [LeetCode 1345](https://leetcode.com/problems/jump-game-iv/description/)
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 1345](https://leetcode.com/problems/jump-game-iv/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 1306. Jump Game III](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1306.%20Jump%20Game%20III) 
+# [Github LeetCode 1345. Jump Game IV](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1345.%20Jump%20Game%20IV) 
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n <= 1:
+            return 0
+
+        graph = {}
+        for i in range(n):
+            if arr[i] in graph:
+                graph[arr[i]].append(i)
+            else:
+                graph[arr[i]] = [i]
+
+        curs = [0]  # store current layers 
+        visited = {0}
+        step = 0
+
+        # when current layer exists 
+        while curs:
+            nex = []
+
+            # iterate the layer 
+            for node in curs:
+                # check if reached end
+                if node == n-1:
+                    return step
+
+                # check same value 
+                for child in graph[arr[node]]:
+                    if child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+                # clear the list to prevent redundant search
+                graph[arr[node]].clear()
+
+                # check neighbors
+                for child in [node-1, node+1]:
+                    if 0 <= child < len(arr) and child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+            curs = nex
+            step += 1
+
+        return -1
