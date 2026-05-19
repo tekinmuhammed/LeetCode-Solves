2d630826844106d2862fcab4c58e1b43c7118d0f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-19 20:50:37 +0300

    D-2540

---
+++ Daily Challenges/Daily-Challenges-Jun-19-2026-solution.py
@@ -0,0 +1,7 @@
+# 2540. Minimum Common Value
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 2540](https://leetcode.com/problems/minimum-common-value/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1345. Jump Game IV](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1345.%20Jump%20Game%20IV) 
