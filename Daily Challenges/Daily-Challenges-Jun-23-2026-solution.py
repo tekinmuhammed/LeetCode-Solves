d0c1b0be2a76cb6a0b7ef6e9aeebb5d7d7d9974d
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-23 16:12:19 +0300

    D-1752

---
+++ Daily Challenges/Daily-Challenges-Jun-23-2026-solution.py
@@ -0,0 +1,7 @@
+# 1752. Check if Array Is Sorted and Rotated
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 1752](https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 33. Search in Rotated Sorted Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/33.%20Search%20in%20Rotated%20Sorted%20Array) 
