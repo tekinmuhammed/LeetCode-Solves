d0125896b9e50666c48eb008df4aa3c54d4a3c1c
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-05-24 23:53:16 +0300

    D-1340

---
+++ Daily Challenges/Daily-Challenges-Jun-24-2026-solution.py
@@ -0,0 +1,7 @@
+# 1340. Jump Game V
+
+**Difficulty:** Hard  
+**Problem Link:** [LeetCode 1340](https://leetcode.com/problems/jump-game-v/description/)
+
+# 🧠 Problem Description 
+# [Github LeetCode 1752. Check if Array Is Sorted and Rotated](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/1752.%20Check%20if%20Array%20Is%20Sorted%20and%20Rotated-2) 
