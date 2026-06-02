327a27bac65c7c18a52685ce4fd493c99722721d
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-06-02 14:19:59 +0300

    D-2553

--- Daily Challenges/Daily-Challenges-May-11-2026-solution.py
+++ Daily Challenges/Daily-Challenges-May-11-2026-solution.py
@@ -3,7 +3,7 @@
 # **Difficulty:** Easy
 # **Problem Link:** [LeetCode 2553](https://leetcode.com/problems/separate-the-digits-in-an-array/description/)
 
-# 🧠 Problem Description
+# 🧠 Problem Description 
 # [Github LeetCode 2553. Separate the Digits in an Array](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/2553.%20Separate%20the%20Digits%20in%20an%20Array) 
 
 class Solution:
