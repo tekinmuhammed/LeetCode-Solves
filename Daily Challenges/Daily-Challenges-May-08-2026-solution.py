8c1d299f95d864907cce4fe54960969cdd39f834
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-06-02 14:16:18 +0300

    D-3629

--- Daily Challenges/Daily-Challenges-May-08-2026-solution.py
+++ Daily Challenges/Daily-Challenges-May-08-2026-solution.py
@@ -3,7 +3,7 @@
 # **Difficulty:** Medium
 # **Problem Link:** [LeetCode 3629](https://leetcode.com/problems/minimum-jumps-to-reach-end-via-prime-teleportation/description/)
 
-# 🧠 Problem Description 
+# 🧠 Problem Description
 # [Github LeetCode 3629. Minimum Jumps to Reach End via Prime Teleportation](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3629.%20Minimum%20Jumps%20to%20Reach%20End%20via%20Prime%20Teleportation) 
 
 from collections import defaultdict
