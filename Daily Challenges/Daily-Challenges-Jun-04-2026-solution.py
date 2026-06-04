fe65dc9c067085b529623cdd2f1107f520817114
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-06-04 15:28:22 +0300

    D-3751

--- Daily Challenges/Daily-Challenges-Jun-04-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jun-04-2026-solution.py
@@ -1,6 +1,10 @@
+# 3751. Total Waviness of Numbers in Range I
+
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 3751](https://leetcode.com/problems/total-waviness-of-numbers-in-range-i/description/)
 
 # 🧠 Problem Description  
-# [Github LeetCode 1871. Jump Game VII](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1871.%20Jump%20Game%20VII) 
+# [Github LeetCode 3751. Total Waviness of Numbers in Range I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3751.%20Total%20Waviness%20of%20Numbers%20in%20Range%20I) 
 
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
