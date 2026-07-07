88474e3f5fff7175ad6a460e8ae1c5ab080ed27f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-07-07 22:10:05 +0300

    D-3754

--- Daily Challenges/Daily-Challenges-Jul-07-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-07-2026-solution.py
@@ -1,10 +1,10 @@
 # 3754. Concatenate Non-Zero Digits and Multiply by Sum I
 
-**Difficulty:** Easy
-**Problem Link:** [LeetCode 3754](https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/description/)
+# **Difficulty:** Easy 
+# **Problem Link:** [LeetCode 3754](https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/description/)
 
-# 🧠 Problem Description  
-# [Github LeetCode 3558. Number of Ways to Assign Edge Weights I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3558.%20Number%20of%20Ways%20to%20Assign%20Edge%20Weights%20I)
+# 🧠 Problem Description
+# [Github LeetCode 3754. Concatenate Non-Zero Digits and Multiply by Sum I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3754.%20Concatenate%20Non-Zero%20Digits%20and%20Multiply%20by%20Sum%20I)
 
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
