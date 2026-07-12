1db3c420a423d7c73795f44650b28da62611bb75
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-07-12 09:03:11 +0300

    D-1331

--- Daily Challenges/Daily-Challenges-Jul-12-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-12-2026-solution.py
@@ -1,3 +1,11 @@
+# 1331. Rank Transform of an Array
+
+**Difficulty:** Easy
+**Problem Link:** [LeetCode 1331](https://leetcode.com/problems/rank-transform-of-an-array/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3754. Concatenate Non-Zero Digits and Multiply by Sum I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3754.%20Concatenate%20Non-Zero%20Digits%20and%20Multiply%20by%20Sum%20I)
+
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         # Store the rank for each number in arr
@@ -11,5 +19,3 @@ class Solution:
         for i in range(len(arr)):
             arr[i] = num_to_rank[arr[i]]
         return arr
-# 🧠 Problem Description
-# [Github LeetCode 3754. Concatenate Non-Zero Digits and Multiply by Sum I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3754.%20Concatenate%20Non-Zero%20Digits%20and%20Multiply%20by%20Sum%20I)
