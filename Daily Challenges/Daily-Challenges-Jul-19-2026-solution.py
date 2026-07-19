3232db8367c56150c255b8269eb7446b330ad692
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-07-19 17:07:09 +0300

    D-1081

--- Daily Challenges/Daily-Challenges-Jul-19-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-19-2026-solution.py
@@ -1,3 +1,11 @@
+# 1081. Smallest Subsequence of Distinct Characters
+
+# **Difficulty:** Medium 
+# **Problem Link:** [LeetCode 1081](https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/description/)
+ 
+# 🧠 Problem Description 
+# [Github LeetCode 1081. Smallest Subsequence of Distinct Characters](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1081.%20Smallest%20Subsequence%20of%20Distinct%20Characters)
+ 
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
         vis = [0] * 26
