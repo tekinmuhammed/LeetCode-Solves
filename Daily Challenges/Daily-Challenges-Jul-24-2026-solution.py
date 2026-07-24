30fee0cafcabc7cf4662f60fc7da8920fc2833cf
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-07-24 16:32:32 +0300

    D-3514

--- Daily Challenges/Daily-Challenges-Jul-24-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-24-2026-solution.py
@@ -1,3 +1,22 @@
      
 # 🧠 Problem Description
 # [Github LeetCode 3513. Number of Unique XOR Triplets I](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/3513.%20Number%20of%20Unique%20XOR%20Triplets%20I)
+
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        m = max(nums)
+        u = 1
+        while u <= m:
+            u <<= 1
+        s = [False] * u
+        n = len(nums)
+        for i in range(n):
+            for j in range(i, n):
+                s[nums[i] ^ nums[j]] = True
+        t = [False] * u
+        for x in range(u):
+            if not s[x]:
+                continue
+            for v in nums:
+                t[x ^ v] = True
+        return sum(1 for b in t if b)
