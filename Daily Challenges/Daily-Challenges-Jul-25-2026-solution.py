f9c44b4749d97167a2537ad81fdc779f7b2c27fb
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-07-25 23:36:07 +0300

    D-3536

--- Daily Challenges/Daily-Challenges-Jul-25-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Jul-25-2026-solution.py
@@ -1,3 +1,11 @@
+# 3536. Maximum Product of Two Digits
+
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 3536](https://leetcode.com/problems/maximum-product-of-two-digits/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3536. Maximum Product of Two Digits ](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3536.%20Maximum%20Product%20of%20Two%20Digits)
+
 class Solution:
     def maxProduct(self, n: int) -> int:
         first, second = 0, 0
