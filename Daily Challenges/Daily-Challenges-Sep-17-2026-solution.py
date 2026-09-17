790288594c413aebdd7f432b1d11c18202ef1e52
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-17 10:23:58 +0300

    D-1477

--- Daily Challenges/Daily-Challenges-Sep-17-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-17-2026-solution.py
@@ -1,6 +1,11 @@
+# 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
+
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 1477](https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 1621. Number of Sets of K Non-Overlapping Line Segments](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1621.%20Number%20of%20Sets%20of%20K%20Non-Overlapping%20Line%20Segments)
+# [Github LeetCode 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1477.%20Find%20Two%20Non-overlapping%20Sub-arrays%20Each%20With%20Target%20Sum)
+
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
         pos = {0: -1}
