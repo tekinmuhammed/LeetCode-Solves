51da3ceea32cbc29dff605d7b66df349f1e33f50
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-17 10:24:41 +0300

    D-1621

--- Daily Challenges/Daily-Challenges-Sep-16-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-16-2026-solution.py
@@ -1,6 +1,6 @@
 # 1621. Number of Sets of K Non-Overlapping Line Segments
 
-# **Difficulty:** Medium 
+# **Difficulty:** Medium
 # **Problem Link:** [LeetCode 1621](https://leetcode.com/problems/number-of-sets-of-k-non-overlapping-line-segments/description/)
 
 # 🧠 Problem Description
