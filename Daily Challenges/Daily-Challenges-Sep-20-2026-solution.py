bd54e925b3ef7478bc48bedfa586ec0e1f4a702f
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-20 10:04:16 +0300

    D-3498

--- Daily Challenges/Daily-Challenges-Sep-20-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-20-2026-solution.py
@@ -1,7 +1,10 @@
 # 3498. Reverse Degree of a String
 
-**Difficulty:** Easy 
-**Problem Link:** [LeetCode 3498](https://leetcode.com/problems/reverse-degree-of-a-string/description/)
+# **Difficulty:** Easy
+# **Problem Link:** [LeetCode 3498](https://leetcode.com/problems/reverse-degree-of-a-string/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3498. Reverse Degree of a String](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3498.%20Reverse%20Degree%20of%20a%20String)
 
 class Solution:
     def reverseDegree(self, s: str) -> int:
