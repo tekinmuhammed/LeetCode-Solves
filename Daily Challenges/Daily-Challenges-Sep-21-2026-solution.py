8452d3dff8d06a2045e7dc808ffa27fc51885f36
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-21 09:38:05 +0300

    D-3524

--- Daily Challenges/Daily-Challenges-Sep-21-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-21-2026-solution.py
@@ -1,21 +1,26 @@
+# 3524. Find X Value of Array I
+
+# **Difficulty:** Medium 
+**Problem Link:** [LeetCode 3524](https://leetcode.com/problems/find-x-value-of-array-i/description/)
+
 class Solution:
     def resultArray(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         result = [0] * k
-        # Initial state: no elements have been processed, so no non-empty subarray exists.
+        # Initial state: no  elements have been processed, so no non-empty subarray exists.
         dp = [0] * k
 
         for i in range(n):
-            ndp = [0] * k  # Current state (rolling array).
+            ndp = [0] * k  #  Current state (rolling array).
 
             ndp[nums[i] % k] += 1
 
             for r in range(k):
                 ndp[(r * nums[i]) % k] += dp[r]
 
-            dp = ndp  # Update the state.
+            dp = ndp  #  Update the state.
 
-            # Accumulate the answer.
+            #  Accumulate the answer.
             for r in range(k):
                 result[r] += dp[r]
         return result
