b9e6352172fcfa78dcfc6ed58f0d16ecc1d47c10
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-23 09:12:14 +0300

    D-1658

---
+++ Daily Challenges/Daily-Challenges-Sep-23-2026-solution.py
@@ -0,0 +1,21 @@
+
+# 🧠 Problem Description
+# [Github LeetCode 3525. Find X Value of Array II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/3525.%20Find%20X%20Value%20of%20Array%20II)
+
+class Solution:
+    def minOperations(self, A: List[int], x: int) -> int:
+        k = sum(A) - x
+        if k < 0: return -1 
+        best = -1
+        
+        s = i = 0
+        
+        for j, num in enumerate(A):
+            s += num
+            while s > k:
+                s -= A[i]
+                i += 1  
+            if s == k:
+                best = max(best, j - i + 1)
+
+        return -1 if best < 0 else len(A) - best
