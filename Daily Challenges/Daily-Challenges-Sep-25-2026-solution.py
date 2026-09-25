a941832aaef4f00e1bd126fdd9a4258cf0810c36
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-25 09:01:09 +0300

    D-1096

--- Daily Challenges/Daily-Challenges-Sep-25-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-25-2026-solution.py
@@ -2,3 +2,6 @@
 
 **Difficulty:** Hard 
 **Problem Link:** [LeetCode 1096](https://leetcode.com/problems/brace-expansion-ii/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 3550. Smallest Index With Digit Sum Equal to Index](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3550.%20Smallest%20Index%20With%20Digit%20Sum%20Equal%20to%20Index)
