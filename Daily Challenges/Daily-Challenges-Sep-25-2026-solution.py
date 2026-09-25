b18e5e95b2e3a156e43b4c274a1fa30a2cd46aa3
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-25 09:02:23 +0300

    D-1096

--- Daily Challenges/Daily-Challenges-Sep-25-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-25-2026-solution.py
@@ -1,10 +1,10 @@
 # 1096. Brace Expansion II
 
-**Difficulty:** Hard 
-**Problem Link:** [LeetCode 1096](https://leetcode.com/problems/brace-expansion-ii/description/)
+# **Difficulty:** Hard
+# **Problem Link:** [LeetCode 1096](https://leetcode.com/problems/brace-expansion-ii/description/)
 
 # 🧠 Problem Description
-# [Github LeetCode 3550. Smallest Index With Digit Sum Equal to Index](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Easy/3550.%20Smallest%20Index%20With%20Digit%20Sum%20Equal%20to%20Index)
+# [Github LeetCode 1096. Brace Expansion II](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Hard/1096.%20Brace%20Expansion%20II)
 
 class Solution:
     def braceExpansionII(self, expression: str) -> List[str]:
