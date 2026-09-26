5046bf7ab20196112b0b58d717ee28c75b2577db
Muhammed Tekin <fetih.tekin.7@gmail.com>
2026-09-26 09:22:08 +0300

    D-1807

--- Daily Challenges/Daily-Challenges-Sep-26-2026-solution.py
+++ Daily Challenges/Daily-Challenges-Sep-26-2026-solution.py
@@ -1,7 +1,11 @@
 # 1807. Evaluate the Bracket Pairs of a String
 
-**Difficulty:** Medium 
-**Problem Link:** [LeetCode 1807](https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/description/)
+# **Difficulty:** Medium
+# **Problem Link:** [LeetCode 1807](https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/description/)
+
+# 🧠 Problem Description
+# [Github LeetCode 1807. Evaluate the Bracket Pairs of a String](https://github.com/tekinmuhammed/LeetCode-Solves/tree/main/Medium/1807.%20Evaluate%20the%20Bracket%20Pairs%20of%20a%20String)
+
 class Solution:
     def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
         d = dict(knowledge)
